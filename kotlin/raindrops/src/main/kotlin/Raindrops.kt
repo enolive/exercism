cc32b040dc7a40ba98292c917b7d01c5bae19cfc
Christoph Welcz <christoph@welcz.de>
2017-07-06 22:38:50 +0200

    changed logic to rule list

--- kotlin/raindrops/src/main/kotlin/Raindrops.kt
+++ kotlin/raindrops/src/main/kotlin/Raindrops.kt
@@ -1,10 +1,15 @@
 object Raindrops {
     fun convert(input: Int): String {
-        if (input.isDivisibleBy(3)) {
-            return "Pling"
-        }
-        
-        return input.toString()
+        val rules = sequenceOf(
+                3 to "Pling",
+                5 to "Plang"
+        )
+
+        val result = rules
+                .filter { (denominator, _) -> input.isDivisibleBy(denominator) }
+                .map { (_, result) -> result }
+                .joinToString("")
+        return if (result.isNotEmpty()) result else input.toString()
     }
 
     private fun Int.isDivisibleBy(denominator: Int) = this % denominator == 0
