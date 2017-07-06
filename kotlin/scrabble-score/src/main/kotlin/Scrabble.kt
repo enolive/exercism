abbe244cfd724c19270baa21bc8cf50c5094b33d
Christoph Welcz <christoph@welcz.de>
2017-07-06 22:01:17 +0200

    moved letter to separate file

--- kotlin/scrabble-score/src/main/kotlin/Scrabble.kt
+++ kotlin/scrabble-score/src/main/kotlin/Scrabble.kt
@@ -8,22 +8,3 @@ object Scrabble {
 
 }
 
-class Letter(value: Char) {
-    private val normalizedValue = value.toLowerCase()
-
-    fun score(): Int {
-        return when (normalizedValue) {
-            'a', 'e', 'i', 'l', 'n', 'o', 'r', 's', 't', 'u' -> 1
-            'd', 'g' -> 2
-            'b', 'c', 'm', 'p' -> 3
-            'f', 'h', 'y' -> 4
-            'k' -> 5
-            'j', 'x' -> 8
-            'q', 'z' -> 10
-            else -> {
-                throw IllegalArgumentException("invalid letter '$normalizedValue'")
-            }
-        }
-    }
-
-}
