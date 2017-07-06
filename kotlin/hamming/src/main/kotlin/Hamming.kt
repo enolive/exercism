407de03ee2563a82e7a641f99fafdc62f165a4ed
Christoph Welcz <christoph@welcz.de>
2017-07-06 14:56:30 +0200

    simplified by infix zipping and merging filter and count

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,14 +1,10 @@
 object Hamming {
     fun compute(leftStrand: String, rightStrand: String): Int {
-        requireEqualLength(leftStrand, rightStrand)
-        return (leftStrand zip rightStrand)
-                .count { it.first != it.second }
-    }
-
-    private fun requireEqualLength(leftStrand: String, rightStrand: String) {
         require (leftStrand.length == rightStrand.length) {
             "leftStrand and rightStrand must be of equal length."
         }
+        return (leftStrand zip rightStrand)
+                .count { it.first != it.second }
     }
 
 }
