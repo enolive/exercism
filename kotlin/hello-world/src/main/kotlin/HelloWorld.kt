779bf89baad8303eb96844942a7d475f8721fc2f
Christoph Welcz <christoph@welcz.de>
2017-07-06 11:19:14 +0200

    Initial setup

--- kotlin/hello-world/src/main/kotlin/HelloWorld.kt
+++ kotlin/hello-world/src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,7 @@
 fun hello(name: String = ""): String {
+   if (name != "") {
+      return "Hello, $name!"
+   }
+   
    return "Hello, World!"
 }
--- kotlin/hello-world/src/test/kotlin/HelloWorldTest.kt
+++ kotlin/hello-world/src/test/kotlin/HelloWorldTest.kt
@@ -11,7 +11,6 @@ class HelloWorldTest {
     }
 
     @Test
-    @Ignore
     fun helloSampleName() {
         assertEquals("Hello, Alice!", hello("Alice"))
     }
