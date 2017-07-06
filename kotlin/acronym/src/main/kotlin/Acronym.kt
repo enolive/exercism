c0297698f515c235f3363b066bcdeeea62e1febc
Christoph Welcz <christoph@welcz.de>
2017-07-06 20:03:55 +0200

    improved readability of wordStartsAt

--- kotlin/acronym/src/main/kotlin/Acronym.kt
+++ kotlin/acronym/src/main/kotlin/Acronym.kt
@@ -7,17 +7,18 @@ object Acronym {
     }
 
     private fun String.wordStartsAt(i: Int): Boolean {
-        if (i == 0) {
-            return true
+        when {
+            firstLetterInSentence(i) -> return true
+            previousIsBlank(i) -> return true
+            previousIsLowerCase(i) -> return true
+            else -> return false
         }
-        val wordBoundaries = arrayOf(' ', '-')
-        if (this[i - 1] in wordBoundaries) {
-            return true
-        }
-        if (this[i].isUpperCase() && this[i - 1].isLowerCase()) {
-            return true
-        }
-        
-        return false
+
     }
+
+    private fun String.previousIsLowerCase(i: Int) = this[i].isUpperCase() && this[i - 1].isLowerCase()
+
+    private fun String.previousIsBlank(i: Int) = this[i - 1] in arrayOf(' ', '-')
+
+    private fun firstLetterInSentence(i: Int) = i == 0
 }
