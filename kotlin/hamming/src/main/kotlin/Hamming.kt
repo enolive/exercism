86af9a84f78268958b2d015239a9e6a46c752350
Christoph Welcz <christoph@welcz.de>
2017-07-06 13:50:31 +0200

    renaming

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,13 +1,13 @@
 object Hamming {
     fun compute(leftStrand: String, rightStrand: String): Int {
-        throwOnLengthMismatch(leftStrand, rightStrand)
+        requireEqualLength(leftStrand, rightStrand)
         return leftStrand
                 .zip(rightStrand)
                 .filter { (left, right) -> left != right }
                 .count()
     }
 
-    private fun throwOnLengthMismatch(leftStrand: String, rightStrand: String) {
+    private fun requireEqualLength(leftStrand: String, rightStrand: String) {
         require (leftStrand.length == rightStrand.length) {
             "leftStrand and rightStrand must be of equal length."
         }
