e74765d61590f93aaa0887649d120a54aa3af441
Christoph Welcz <christoph@welcz.de>
2017-07-06 17:54:25 +0200

    changed to pattern matching

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,12 +1,15 @@
+import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit.*
+import java.time.temporal.Temporal
 
-class Gigasecond(birth: LocalDateTime) {
-    private val amount = Math.pow(10.0, 9.0).toLong()
+class Gigasecond(birth: Temporal) {
+    private val amount = Duration.of(1_000_000_000, SECONDS)
 
-    val date: LocalDateTime = birth.plus(amount, SECONDS)
-
-    constructor(birth: LocalDate) : this(birth.atStartOfDay())
-
+    val date: LocalDateTime = when(birth) {
+        is LocalDate -> birth.atStartOfDay().plus(amount)
+        is LocalDateTime -> birth.plus(amount)
+        else -> throw IllegalArgumentException()
+    }    
 }
