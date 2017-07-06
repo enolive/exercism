6c0e60b623a21dd0fe3b9515ae5f3eb5ee81a2e3
Christoph Welcz <christoph@welcz.de>
2017-07-06 13:42:17 +0200

    simplified solution

--- kotlin/hamming/src/main/kotlin/Hamming.kt
+++ kotlin/hamming/src/main/kotlin/Hamming.kt
@@ -1,10 +1,9 @@
 object Hamming {
     fun compute(first: String, second: String): Int {
-        if (first != second) {
-            return first.zip(second).filter { (f, s) -> f != s }.count()
-        }
-
-        return 0
+        return first
+                .zip(second)
+                .filter { (f, s) -> f != s }
+                .count()
     }
 
 }
