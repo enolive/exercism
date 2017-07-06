d440376c6cf4300ea846079f45c09a5474b5ed1a
Christoph Welcz <christoph@welcz.de>
2017-07-06 17:56:57 +0200

    using duration

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -1,11 +1,12 @@
+import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit.*
 
 class Gigasecond(birth: LocalDateTime) {
-    private val amount = Math.pow(10.0, 9.0).toLong()
+    private val amount = Duration.of(1_000_000_000, SECONDS)
 
-    val date: LocalDateTime = birth.plus(amount, SECONDS)
+    val date: LocalDateTime = birth.plus(amount)
 
     constructor(birth: LocalDate) : this(birth.atStartOfDay())
 
