04eb29f622b3ff0e7fc750a4c4299919c864b68a
Christoph Welcz <christoph@welcz.de>
2017-07-07 02:21:27 +0200

    sumBy instead of filter, sum

--- kotlin/difference-of-squares/src/main/kotlin/Squares.kt
+++ kotlin/difference-of-squares/src/main/kotlin/Squares.kt
@@ -1,7 +1,7 @@
 class Squares(private val i: Int) {
     fun squareOfSum(): Int = (1..i).sum().square()
 
-    fun sumOfSquares(): Int = (1..i).map { it.square() }.sum()
+    fun sumOfSquares(): Int = (1..i).sumBy { it.square() }
 
     fun difference(): Int = squareOfSum() - sumOfSquares()
 
