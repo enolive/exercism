0699d0844c0107d3b47ddd99c0cea2f2557ed864
Christoph Welcz <christoph@welcz.de>
2017-07-09 14:27:57 +0200

    some refactoring

--- kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
+++ kotlin/sum-of-multiples/src/main/kotlin/SumOfMultiples.kt
@@ -1,8 +1,12 @@
 object SumOfMultiples {
-    fun sum(quotients: Set<Int>, number: Int): Int {
+    fun sum(quotients: Iterable<Int>, number: Int): Int {
         return (1..number - 1)
-                .filter { n -> quotients.any { q -> n % q == 0 } }
+                .filter { n -> n.isMultipleOfAny(quotients) }
                 .sum()
     }
+
+    private fun Int.isMultipleOfAny(quotients: Iterable<Int>) = quotients.any { q -> isDivisibleBy(q) }
+
+    private fun Int.isDivisibleBy(q: Int) = this % q == 0
 
 }
