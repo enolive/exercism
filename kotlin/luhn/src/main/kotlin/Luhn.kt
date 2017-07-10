821cb24be7c4da322fa8814445258e2f848ae28c
Christoph Welcz <christoph@welcz.de>
2017-07-10 21:05:43 +0200

    extracted some functions to lessen the brainfuck

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -3,11 +3,25 @@ object Luhn {
         if (numberCode.length < 2) {
             return false
         }
-        
-        return numberCode.reversed()
-                .map { it.toInt() - '0'.toInt() }
-                .mapIndexed { index, digit -> if ((index + 1) % 2 == 0) digit * 2 else digit }
-                .map { if (it > 9) it - 9 else it }
-                .sum() % 10 == 0
+
+        return numberCode
+                .reversedNumberSequence()
+                .mapIndexed { index, digit -> doubleEverySecond(index + 1, digit) }
+                .map { subtractWhenDoubleDigit(it) }
+                .sum()
+                .isDivisibleBy(10)
     }
+
+    private fun String.reversedNumberSequence() =
+            reversed().map { it.parseCharacter() }
+
+    private fun doubleEverySecond(index: Int, digit: Int) =
+            if (index.isDivisibleBy(2)) digit * 2 else digit
+
+    private fun Int.isDivisibleBy(divisor: Int) = (this) % divisor == 0
+
+    private fun subtractWhenDoubleDigit(it: Int) = 
+            if (it > 9) it - 9 else it
+
+    private fun Char.parseCharacter() = toInt() - '0'.toInt()
 }
