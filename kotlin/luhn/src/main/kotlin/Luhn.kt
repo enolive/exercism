8b732d503405709f31eb98f90927e6ceaba55219
Christoph Welcz <christoph@welcz.de>
2017-07-10 22:20:47 +0200

    final version

--- kotlin/luhn/src/main/kotlin/Luhn.kt
+++ kotlin/luhn/src/main/kotlin/Luhn.kt
@@ -4,27 +4,31 @@ object Luhn {
         if (withoutWhitespaces.hasLexicalErrors()) {
             return false
         }
-        
-        return checkSumOf(withoutWhitespaces).isDivisibleBy(10)
+
+        return checkSumOf(withoutWhitespaces) isDivisibleBy 10
     }
 
     private fun String.hasLexicalErrors()
             = length < 2 || any { it !in '0'..'9' }
 
-    private fun checkSumOf(code: String): Int 
+    private fun checkSumOf(code: String): Int
             = code.reversedNumberSequence()
-            .mapIndexed(this::double)
-            .map(this::rollOver)
+            .mapIndexed { index, digit ->
+                when {
+                    (index + 1) isDivisibleBy 2 -> doubleWithRollover(digit)
+                    else -> digit
+                }
+            }
             .sum()
+
+    private fun doubleWithRollover(digit: Int): Int {
+        val doubled = digit * 2
+        return if (doubled > 9) doubled - 9 else doubled
+    }
 
     private fun String.reversedNumberSequence() =
             reversed().map { "$it".toInt() }
 
-    private fun double(index: Int, digit: Int) =
-            if ((index + 1).isDivisibleBy(2)) digit * 2 else digit
-
-    private fun rollOver(it: Int) = if (it > 9) it - 9 else it
-
-    private fun Int.isDivisibleBy(divisor: Int) = (this) % divisor == 0
+    private infix fun Int.isDivisibleBy(divisor: Int) = (this) % divisor == 0
 
 }
