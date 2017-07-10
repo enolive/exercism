3a8007af5c34947df28af960f02e7c84eac4045f
Christoph Welcz <christoph@welcz.de>
2017-07-10 22:38:40 +0200

    replaced default assertions with AssertJ. Looks so much better

--- kotlin/luhn/src/test/kotlin/LuhnTest.kt
+++ kotlin/luhn/src/test/kotlin/LuhnTest.kt
@@ -1,79 +1,78 @@
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
 
 class LuhnTest {
 
     @Test
     fun `single digit strings cannot be valid`() {
-        assertFalse(Luhn.isValid("1"))
+        assertThat(Luhn.isValid("1")).isFalse()
     }
 
     @Test
     fun `single zero is invalid`() {
-        assertFalse(Luhn.isValid("0"))
+        assertThat(Luhn.isValid("0")).isFalse()
     }
 
     @Test
     fun `simple valid SIN that remains valid if reversed`() {
-        assertTrue(Luhn.isValid("059"))
-        assertTrue(Luhn.isValid("950"))
+        assertThat(Luhn.isValid("059")).isTrue()
+        assertThat(Luhn.isValid("950")).isTrue()
     }
 
     @Test
     fun `simple valid SIN that becomes invalid if reversed`() {
-        assertTrue(Luhn.isValid("59"))
-        assertFalse(Luhn.isValid("95"))
+        assertThat(Luhn.isValid("59")).isTrue()
+        assertThat(Luhn.isValid("95")).isFalse()
     }
 
     @Test
     fun `valid canadian SIN`() {
-        assertTrue(Luhn.isValid("055 444 285"))
+        assertThat(Luhn.isValid("055 444 285")).isTrue()
     }
 
     @Test
     fun `invalid canadian SIN`() {
-        assertFalse(Luhn.isValid("055 444 286"))
+        assertThat(Luhn.isValid("055 444 286")).isFalse()
     }
 
     @Test
     fun `invalid credit card`() {
-        assertFalse(Luhn.isValid("8273 1232 7352 0569"))
+        assertThat(Luhn.isValid("8273 1232 7352 0569")).isFalse()
     }
 
     @Test
     fun `valid strings with non digit included become invalid`() {
-        assertFalse(Luhn.isValid("055a 444 285"))
+        assertThat(Luhn.isValid("055a 444 285")).isFalse()
     }
 
     @Test
     fun `valid strings with punctuation included become invalid`() {
-        assertFalse(Luhn.isValid("055-444-285"))
+        assertThat(Luhn.isValid("055-444-285")).isFalse()
     }
 
     @Test
     fun `valid strings with commas included become invalid`() {
-        assertFalse(Luhn.isValid("055,444,285"))
+        assertThat(Luhn.isValid("055,444,285")).isFalse()
     }
 
     @Test
     fun `valid strings with symbols included become invalid`() {
-        assertFalse(Luhn.isValid("055£ 444$ 285"))
+        assertThat(Luhn.isValid("055£ 444$ 285")).isFalse()
     }
 
     @Test
     fun `single zero with space is invalid`() {
-        assertFalse(Luhn.isValid(" 0"))
+        assertThat(Luhn.isValid(" 0")).isFalse()
     }
 
     @Test
     fun `more than single zero is valid`() {
-        assertTrue(Luhn.isValid("0000 0"))
+        assertThat(Luhn.isValid("0000 0")).isTrue()
     }
 
     @Test
     fun `input digit 9 is correctly converted to output digit9`() {
-        assertTrue(Luhn.isValid("091"))
+        assertThat(Luhn.isValid("091")).isTrue()
     }
 
 }
