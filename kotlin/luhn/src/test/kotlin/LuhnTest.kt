2d1ca3c9019e0ac5e8b03fa61fbb98b172136267
Christoph Welcz <christoph@welcz.de>
2017-07-10 21:36:20 +0200

    beautified tests by using real names instead of pascal cased mess

--- kotlin/luhn/src/test/kotlin/LuhnTest.kt
+++ kotlin/luhn/src/test/kotlin/LuhnTest.kt
@@ -6,76 +6,75 @@ import kotlin.test.assertTrue
 
 class LuhnTest {
 
-
     @Test
-    fun singleDigitStringsCannotBeValid() {
+    fun `single digit strings cannot be valid`() {
         assertFalse(Luhn.isValid("1"))
     }
 
     @Test
-    fun singleZeroIsInvalid() {
+    fun `single zero is invalid`() {
         assertFalse(Luhn.isValid("0"))
     }
 
     @Test
-    fun simpleValidSINThatRemainsValidIfReversed() {
+    fun `simple valid SIN that remains valid if reversed`() {
         assertTrue(Luhn.isValid("059"))
         assertTrue(Luhn.isValid("950"))
     }
 
     @Test
-    fun simpleValidSINThatBecomesInvalidIfReversed() {
+    fun `simple valid SIN that becomes invalid if reversed`() {
         assertTrue(Luhn.isValid("59"))
         assertFalse(Luhn.isValid("95"))
     }
 
     @Test
-    fun validCanadianSIN() {
+    fun `valid canadian SIN`() {
         assertTrue(Luhn.isValid("055 444 285"))
     }
 
     @Test
-    fun invalidCanadianSIN() {
+    fun `invalid canadian SIN`() {
         assertFalse(Luhn.isValid("055 444 286"))
     }
 
     @Test
-    fun invalidCreditCard() {
+    fun `invalid credit card`() {
         assertFalse(Luhn.isValid("8273 1232 7352 0569"))
     }
 
     @Test
-    fun validStringsWithNonDigitIncludedBecomeInvalid() {
+    fun `valid strings with non digit included become invalid`() {
         assertFalse(Luhn.isValid("055a 444 285"))
     }
 
     @Test
-    fun validStringsWithPunctuationIncludedBecomeInvalid() {
+    fun `valid strings with punctuation included become invalid`() {
         assertFalse(Luhn.isValid("055-444-285"))
     }
 
     @Test
-    fun validStringsWithAlternativePunctuationIncludedBecomeInvalid() {
+    fun `valid strings with commas included become invalid`() {
         assertFalse(Luhn.isValid("055,444,285"))
     }
 
     @Test
-    fun validStringsWithSymbolsIncludedBecomeInvalid() {
+    fun `valid strings with symbols included become invalid`() {
         assertFalse(Luhn.isValid("055£ 444$ 285"))
     }
 
     @Test
-    fun singleZeroWithSpaceIsInvalid() {
+    fun `single zero with space is invalid`() {
         assertFalse(Luhn.isValid(" 0"))
     }
 
     @Test
-    fun moreThanSingleZeroIsValid() {
+    fun `more than single zero is valid`() {
         assertTrue(Luhn.isValid("0000 0"))
     }
 
     @Test
-    fun inputDigit9IsCorrectlyConvertedToOutputDigit9() {
+    fun `input digit 9 is correctly converted to output digit9`() {
         assertTrue(Luhn.isValid("091"))
     }
 
