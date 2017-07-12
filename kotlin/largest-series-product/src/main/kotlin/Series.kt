7379aec6832152ea0a53359789ff3af3d24c6161
Christoph Welcz <christoph@welcz.de>
2017-07-12 14:23:07 +0200

    Solution for sequence of 2 ready

--- kotlin/largest-series-product/src/main/kotlin/Series.kt
+++ kotlin/largest-series-product/src/main/kotlin/Series.kt
@@ -1,10 +1,20 @@
-class Series(input: String) {
+class Series(private val input: String) {
     init {
         require(input.all { it.isDigit() })
     }
 
     fun getLargestProduct(span: Int): Long {
-        return 72
+        val max = input.indices
+                .filter { it < input.length - 1 }
+                .map { Pair(input[it], input[it + 1]) }
+                .map { it.toLong() }
+                .map { it.first * it.second }
+                .max()
+        return max!!
     }
 
+}
+
+private fun Pair<Char, Char>.toLong(): Pair<Long, Long> {
+    return Pair(this.first.toString().toLong(), this.second.toString().toLong())
 }
