abf57fa6855a7b1284d95871eae9b5a8e79ad1a4
Christoph Welcz <christoph@welcz.de>
2017-07-12 11:27:23 +0200

    some refactorings

--- kotlin/triangle/src/main/kotlin/Triangle.kt
+++ kotlin/triangle/src/main/kotlin/Triangle.kt
@@ -1,24 +1,22 @@
-class Triangle {
-    private val first: Double
-    private val second: Double
-    private val third: Double
+class Triangle(private val first: Double, private val second: Double, private val third: Double) {
 
-    constructor(first: Double, second: Double, third: Double) {
-        require(first > 0.0 && second > 0.0 && third > 0.0)
-        require(first + second >= third && second + third >= first && first + third >= second)
-        this.first = first
-        this.second = second
-        this.third = third
-    }
-
-    constructor(first: Int, second: Int, third: Int) 
+    constructor(first: Int, second: Int, third: Int)
             : this(first.toDouble(), second.toDouble(), third.toDouble())
 
+    init {
+        require(validSideLengths())
+        require(triangularInequality())
+    }
+
+    private fun validSideLengths() = first > 0.0 && second > 0.0 && third > 0.0
+
+    private fun triangularInequality() = first + second >= third && second + third >= first && first + third >= second
     val isEquilateral: Boolean
         get() {
             return (first == second && first == third)
         }
     val isScalene: Boolean = false
+
     val isIsosceles: Boolean
         get() {
             return !(first != second && first != third && second != third)
