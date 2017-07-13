de1fe9e8d4d606bda29c1f3591440a7bc9e320cc
Christoph Welcz <christoph@welcz.de>
2017-07-13 17:10:55 +0200

    removed temporary value

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,8 +1,7 @@
 object HandshakeCalculator {
     fun calculateHandshake(i: Int): List<Signal> {
-        val signals = Signal.values()
+        return Signal.values()
                 .filter { i and it.bitmask != 0 }
                 .let { if (i and 16 != 0) it.reversed() else it }
-        return signals
     }
 }
