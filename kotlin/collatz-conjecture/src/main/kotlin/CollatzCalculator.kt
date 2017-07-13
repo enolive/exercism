c9e8d68775d500091004c5799cd0963133f3d44f
Christoph Welcz <christoph@welcz.de>
2017-07-13 20:18:21 +0200

    moved tail recursion to nested function

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,17 +1,20 @@
 object CollatzCalculator {
-    tailrec fun computeStepCount(i: Int, count: Int = 0): Int {
+    fun computeStepCount(i: Int): Int {
+        tailrec fun computeIt(i: Int, count: Int): Int {
+            if (i == 1) {
+                return count
+            }
+            return computeIt(when {
+                i divisibleBy 2 -> 3 * i + 1
+                else -> i / 2
+            }, count + 1)
+        }
+
         require(i > 0) {
             "Only natural numbers are allowed"
         }
-        
-        if (i == 1) {
-            return count
-        }
 
-        return computeStepCount(when {
-            i divisibleBy 2 -> 3 * i + 1
-            else -> i / 2
-        }, count + 1)
+        return computeIt(i, 0)
     }
 
     private infix fun Int.divisibleBy(i: Int) = rem(i) != 0
