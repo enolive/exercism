1efc229f088d365c4abaa7eaa613dffa610aca1a
Christoph Welcz <christoph@welcz.de>
2017-07-13 22:28:41 +0200

    refactored a bit

--- kotlin/nth-prime/src/main/kotlin/Prime.kt
+++ kotlin/nth-prime/src/main/kotlin/Prime.kt
@@ -4,6 +4,14 @@ object Prime {
         return primes.elementAt(i - 1)
     }
 
-    private fun primesUpto(i: Int) = sequenceOf(2, 3, 5, 7, 11, 13)
+    private fun primesUpto(i: Int): List<Int> {
+        return (2..13).filter { it.isPrime() }
+    }
 
 }
+
+private fun Int.isPrime(): Boolean {
+    return (2..upperLimit()).all { this % it != 0 }
+}
+
+private fun Int.upperLimit() = Math.sqrt(this.toDouble()).toInt()
