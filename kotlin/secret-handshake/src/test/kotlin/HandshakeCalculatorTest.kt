ccef9ba9d8c462d373b6e1f87146994ab0da8c49
Christoph Welcz <christoph@welcz.de>
2017-07-13 15:48:56 +0200

    small cleanup

--- kotlin/secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
+++ kotlin/secret-handshake/src/test/kotlin/HandshakeCalculatorTest.kt
@@ -1,4 +1,4 @@
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 
 /*
@@ -6,92 +6,66 @@ import org.junit.Test
  */
 class `Calculate Handshake` {
 
-    fun assertEquals(expected: List<Signal>, actual: List<Signal>) {
-        Assertions.assertThat(actual).isEqualTo(expected)
-    }
-
     @Test
     fun `input 1 yields a wink`() {
-        assertEquals(
-                listOf(Signal.WINK),
-                HandshakeCalculator.calculateHandshake(1))
+        assertThat(HandshakeCalculator.calculateHandshake(1)).isEqualTo(listOf(Signal.WINK))
     }
 
     @Test
     fun `input 2 yields a double blink`() {
-        assertEquals(
-                listOf(Signal.DOUBLE_BLINK),
-                HandshakeCalculator.calculateHandshake(2))
+        assertThat(HandshakeCalculator.calculateHandshake(2)).isEqualTo(listOf(Signal.DOUBLE_BLINK))
     }
 
     @Test
     fun `input 4 yields a close your eyes`() {
-        assertEquals(
-                listOf(Signal.CLOSE_YOUR_EYES),
-                HandshakeCalculator.calculateHandshake(4))
+        assertThat(HandshakeCalculator.calculateHandshake(4)).isEqualTo(listOf(Signal.CLOSE_YOUR_EYES))
     }
 
     @Test
     fun `input 8 yields a jump`() {
-        assertEquals(
-                listOf(Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(8))
+        assertThat(HandshakeCalculator.calculateHandshake(8)).isEqualTo(listOf(Signal.JUMP))
     }
 
     @Test
     fun `input that yields two actions`() {
-        assertEquals(
-                listOf(Signal.WINK, Signal.DOUBLE_BLINK),
-                HandshakeCalculator.calculateHandshake(3))
+        assertThat(HandshakeCalculator.calculateHandshake(3)).isEqualTo(listOf(Signal.WINK, Signal.DOUBLE_BLINK))
     }
 
     @Test
     fun `input that yields two reversed actions`() {
-        assertEquals(
-                listOf(Signal.DOUBLE_BLINK, Signal.WINK),
-                HandshakeCalculator.calculateHandshake(19))
+        assertThat(HandshakeCalculator.calculateHandshake(19)).isEqualTo(listOf(Signal.DOUBLE_BLINK, Signal.WINK))
     }
 
     @Test
     fun `a single action yields the same action`() {
-        assertEquals(
-                listOf(Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(24))
+        assertThat(HandshakeCalculator.calculateHandshake(24)).isEqualTo(listOf(Signal.JUMP))
     }
 
     @Test
     fun `reversing no actions yields no actions`() {
-        assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(16))
+        assertThat(HandshakeCalculator.calculateHandshake(16)).isEmpty()
     }
 
     @Test
     fun `input that yields all actions`() {
-        assertEquals(
-                listOf(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP),
-                HandshakeCalculator.calculateHandshake(15))
+        assertThat(HandshakeCalculator.calculateHandshake(15)).
+                isEqualTo(listOf(Signal.WINK, Signal.DOUBLE_BLINK, Signal.CLOSE_YOUR_EYES, Signal.JUMP))
     }
 
     @Test
     fun `input that yields all actions reversed`() {
-        assertEquals(
-                listOf(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK),
-                HandshakeCalculator.calculateHandshake(31))
+        assertThat(HandshakeCalculator.calculateHandshake(31))
+                .isEqualTo(listOf(Signal.JUMP, Signal.CLOSE_YOUR_EYES, Signal.DOUBLE_BLINK, Signal.WINK))
     }
 
     @Test
     fun `input 0 yields no actions`() {
-        assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(0))
+        assertThat(HandshakeCalculator.calculateHandshake(0)).isEmpty()
     }
 
     @Test
     fun `input with lower 5 bits not set yields no actions`() {
-        assertEquals(
-                emptyList(),
-                HandshakeCalculator.calculateHandshake(32))
+        assertThat(HandshakeCalculator.calculateHandshake(32)).isEmpty()
     }
 
 }
