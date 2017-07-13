d4a307aef5a10b3a2a034d56f3e2a16a2ddb8eba
Christoph Welcz <christoph@welcz.de>
2017-07-13 17:09:55 +0200

    infix notation

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,8 +1,8 @@
 object HandshakeCalculator {
     fun calculateHandshake(i: Int): List<Signal> {
         val signals = Signal.values()
-                .filter { i.and(it.bitmask) != 0 }
-                .let { if (i.and(16) != 0) it.reversed() else it }
+                .filter { i and it.bitmask != 0 }
+                .let { if (i and 16 != 0) it.reversed() else it }
         return signals
     }
 }
