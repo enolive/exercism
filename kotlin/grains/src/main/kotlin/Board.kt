b53c5d6fec2b040b1452723b90a5adfbc427f252
Christoph Welcz <christoph@welcz.de>
2017-07-13 19:49:07 +0200

    solved with geometric sum

--- kotlin/grains/src/main/kotlin/Board.kt
+++ kotlin/grains/src/main/kotlin/Board.kt
@@ -1,16 +1,19 @@
 import java.math.BigInteger
+import java.math.BigInteger.valueOf
 
 object Board {
     fun getGrainCountForSquare(i: Int): BigInteger {
         require(i in 1..64) {
             "Only integers between 1 and 64 (inclusive) are allowed."
         }
-        return BigInteger.valueOf(2).pow(i - 1)
+        return valueOf(2).pow(i - 1)
     }
 
     fun getTotalGrainCount(): BigInteger {
-        return (1..64)
-                .map { getGrainCountForSquare(it) }
-                .reduce { sum, number -> sum + number }
+        return geometricSum(2, 64)
     }
+
+    private fun geometricSum(exponent: Long, count: Int) =
+            (valueOf(exponent).pow(count) - valueOf(1)) /
+                    (valueOf(exponent) - valueOf(1))
 }
