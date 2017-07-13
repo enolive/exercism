fa03249067d9e878f198e772e27f710f123cd8e5
Christoph Welcz <christoph@welcz.de>
2017-07-13 15:57:17 +0200

    using of let instead of conditional return

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -2,12 +2,7 @@ object HandshakeCalculator {
     fun calculateHandshake(i: Int): List<Signal> {
         val signals = Signal.values()
                 .filter { i.and(it.bitmask) != 0 }
-        
-        if (i.and(16) != 0) {
-            return signals.reversed()
-        }
-        
+                .let { if (i.and(16) != 0) it.reversed() else it }
         return signals
     }
-
 }
