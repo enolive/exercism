37cf31b4d578d6b047e365036e4d699398306dda
Christoph Welcz <christoph@welcz.de>
2017-07-13 20:48:49 +0200

    made  the solution a little bit cleaner

--- kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
+++ kotlin/collatz-conjecture/src/main/kotlin/CollatzCalculator.kt
@@ -1,21 +1,25 @@
 object CollatzCalculator {
     fun computeStepCount(i: Int): Int {
-        tailrec fun computeIt(i: Int, count: Int): Int {
-            if (i == 1) {
-                return count
-            }
-            return computeIt(when {
-                i.isEven() -> i / 2
-                else -> 3 * i + 1
-            }, count + 1)
-        }
-
         require(i > 0) {
             "Only natural numbers are allowed"
+        }
+
+        tailrec fun computeIt(i: Int, count: Int): Int {
+            return when (i) {
+                1 -> count
+                else -> computeIt(i.next(), count + 1)
+            }
         }
 
         return computeIt(i, 0)
     }
     
+    private fun Int.next(): Int {
+        return when {
+            isEven() -> this / 2
+            else -> 3 * this + 1
+        }
+    }
+
     private fun Int.isEven() = this.rem(2) == 0
 }
