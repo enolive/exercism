3bdefc1f43858ab17555d5461d0ae7ab0662f954
Christoph Welcz <christoph@welcz.de>
2017-07-13 15:16:28 +0200

    filtered loop

--- kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
+++ kotlin/secret-handshake/src/main/kotlin/HandshakeCalculator.kt
@@ -1,23 +1,8 @@
 object HandshakeCalculator {
     fun calculateHandshake(i: Int): List<Signal> {
-        val list = mutableListOf<Signal>()
-        if (i.and(1) != 0) {
-            list.add(Signal.WINK)
-        }
-        
-        if (i.and(2) != 0) {
-            list.add(Signal.DOUBLE_BLINK)
-        }
-        
-        if (i.and(4) != 0) {
-            list.add(Signal.CLOSE_YOUR_EYES)
-        }
+        return Signal.values()
+                .filter { i.and(it.bitmask) != 0 }
 
-        if (i.and(8) != 0) {
-            list.add(Signal.JUMP)
-        }
-        
-        return list
     }
 
 }
