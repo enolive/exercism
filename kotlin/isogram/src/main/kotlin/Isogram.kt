776b06079441c8c34dd6f8349420ec8956b025b7
Christoph Welcz <christoph@welcz.de>
2017-07-16 20:00:01 +0200

    ignore hyphens

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -2,6 +2,7 @@ object Isogram {
     fun isIsogram(input: String): Boolean {
         val characterCount = input
                 .toCharArray()
+                .filter { it != '-' }
                 .map { it.toUpperCase() }
                 .groupBy { it }
         return characterCount.all { it.value.count() == 1 }
