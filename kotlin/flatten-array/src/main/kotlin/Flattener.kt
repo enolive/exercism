54edcb89e69a2840e38feac5d4d5baa57049fdce
Christoph Welcz <christoph@welcz.de>
2017-07-16 21:02:46 +0200

    made code green

--- kotlin/flatten-array/src/main/kotlin/Flattener.kt
+++ kotlin/flatten-array/src/main/kotlin/Flattener.kt
@@ -1,30 +1,24 @@
 object Flattener {
     fun flatten(list: List<Any?>): List<Any> {
-        fun flattenIt(list: List<Any?>, acc: List<Any>): List<Any> {
-            if (list.isEmpty()) {
+        fun flattenIt(remainingList: List<Any?>, acc: List<Any>): List<Any> {
+            if (remainingList.isEmpty()) {
                 return acc
             }
 
-            val nextElement = list
+            val (howManyToDrop, firstValue) = remainingList
                     .indices
-                    .filter { list[it] != null }
-                    .map { Pair(it + 1, list[it] as Any) }
-                    .firstOrNull()
-            if (nextElement == null) {
-                return acc
-            }
-            
-            val (howManyToDrop, first) = nextElement
-            
-            if (first is List<*>) {
-                return flattenIt(list.drop(howManyToDrop), acc + flattenIt(first, emptyList()))
+                    .filter { remainingList[it] != null }
+                    .map { Pair(it + 1, remainingList[it] as Any) }
+                    .firstOrNull() ?: return acc
+
+            if (firstValue is List<*>) {
+                return flattenIt(remainingList.drop(howManyToDrop), acc + flattenIt(firstValue, emptyList()))
             }
 
-            return flattenIt(list.drop(howManyToDrop), acc + first)
+            return flattenIt(remainingList.drop(howManyToDrop), acc + firstValue)
         }
 
-        val result = emptyList<Any>()
-        return flattenIt(list, result)
+        return flattenIt(list, emptyList())
     }
 
 }
