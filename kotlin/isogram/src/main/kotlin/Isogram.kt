77b1aa4088342b7d2179793a214cf07f01805de2
Christoph Welcz <christoph@welcz.de>
2017-07-16 20:05:51 +0200

    use an easier filter condition

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -2,7 +2,7 @@ object Isogram {
     fun isIsogram(input: String): Boolean {
         val characterCount = input
                 .toCharArray()
-                .filter { it !in listOf(' ', '-') }
+                .filter { it.isLetter() }
                 .map { it.toUpperCase() }
                 .groupBy { it }
         return characterCount.all { it.value.size == 1 }
