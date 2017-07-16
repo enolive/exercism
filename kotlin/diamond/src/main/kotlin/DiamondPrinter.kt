c99c5f189b853cb27107903051efbb1bd4d0f0ff
Christoph Welcz <christoph@welcz.de>
2017-07-16 18:50:12 +0200

    just calculate the upper half

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,9 +1,8 @@
 class DiamondPrinter {
     fun printToList(limit: Char): List<String> {
-        return range(limit).map { line(it, limit) }
+        val upperHalf = (('A'..limit)).map { line(it, limit) }
+        return upperHalf + upperHalf.reversed().drop(1)
     }
-
-    private fun range(limit: Char) = (('A'..limit) + (limit - 1 downTo 'A'))
 
     private fun line(current: Char, limit: Char): String {
         val numberOfEdgeSpaces = limit - current
