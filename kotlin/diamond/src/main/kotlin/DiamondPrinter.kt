554a75d120df19f79c7879056d8544e19c4f0855
Christoph Welcz <christoph@welcz.de>
2017-07-16 18:28:00 +0200

    now completely generalized

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,35 +1,17 @@
 class DiamondPrinter {
     fun printToList(c: Char): List<String> {
-        if (c == 'E') {
-            return listOf('A', 'B', 'C', 'D', 'E', 'D', 'C', 'B', 'A')
-                    .map { line(it, c) }
-        }
-        if (c == 'C') {
-            return listOf(
-                    " ".repeat(2) + 'A' + " ".repeat(2),
-                    " ".repeat(1) + 'B' + " ".repeat(1) + 'B' + " ".repeat(1),
-                    " ".repeat(0) + 'C' + " ".repeat(3) + 'C' + " ".repeat(0),
-                    " ".repeat(1) + 'B' + " ".repeat(1) + 'B' + " ".repeat(1),
-                    " ".repeat(2) + 'A' + " ".repeat(2)
-            )
-        }
-        if (c == 'B') {
-            return listOf(
-                    " ".repeat(1) + 'A' + " ".repeat(1),
-                    'B' + " ".repeat(1) + 'B',
-                    " ".repeat(1) + 'A' + " ".repeat(1))
-        }
-
-        return listOf("A")
+        return (('A'..c) + (c - 1 downTo 'A'))
+                .map { line(it, c) }
     }
 
     private fun line(current: Char, limit: Char): String {
         val edges = " ".repeat(limit - current)
-        if (current == 'A') {
+        val numberOfMiddleSpaces = 2 * (current - 'A') - 1
+        if (numberOfMiddleSpaces < 0) {
             return edges + current + edges
         }
-        
-        val middle = " ".repeat(2 * (current - 'A') - 1)
+
+        val middle = " ".repeat(numberOfMiddleSpaces)
         return edges +
                 current +
                 middle +
