7808961fd1802b076235942b714e0c3ed8e33e7e
Christoph Welcz <christoph@welcz.de>
2017-07-16 19:59:00 +0200

    ignore case

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,6 +1,9 @@
 object Isogram {
     fun isIsogram(input: String): Boolean {
-        val characterCount = input.toCharArray().groupBy { it }
+        val characterCount = input
+                .toCharArray()
+                .map { it.toUpperCase() }
+                .groupBy { it }
         return characterCount.all { it.value.count() == 1 }
     }
 
