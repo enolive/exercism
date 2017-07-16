111f21c265c5bb0d2a7767ef7372b6b9a70eab31
Christoph Welcz <christoph@welcz.de>
2017-07-16 19:40:59 +0200

    explaining variables and functions

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -5,10 +5,20 @@ class DiamondPrinter {
     }
 
     private fun line(current: Char, limit: Char): String {
-        val numberOfMiddleSpaces = 2 * (current - 'A') - 1
-        val edges = " ".repeat(limit - current)
-        val middle = if (numberOfMiddleSpaces > 0) " ".repeat(numberOfMiddleSpaces / 2 + 1) else ""
-        val lineHalf = edges + current + middle
+        val lineHalf = edgeSpaces(limit, current) + current + middleSpacesHalf(current)
         return lineHalf + lineHalf.reversed().drop(1)
+    }
+
+    private fun edgeSpaces(limit: Char, current: Char) = " ".repeat(limit - current)
+
+    private fun middleSpacesHalf(current: Char): String {
+        val totalSpaces = 2 * (current - 'A') - 1
+        val middle = if (totalSpaces <= 0) {
+            ""
+        } else {
+            val addOneSpaceForLaterDrop = 1
+            " ".repeat(totalSpaces / 2 + addOneSpaceForLaterDrop)
+        }
+        return middle
     }
 }
