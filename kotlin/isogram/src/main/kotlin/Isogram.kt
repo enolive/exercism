a94da5df7e323a80d89ec649775a848397ef13b2
Christoph Welcz <christoph@welcz.de>
2017-07-16 20:02:19 +0200

    ignore spaces

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -2,7 +2,7 @@ object Isogram {
     fun isIsogram(input: String): Boolean {
         val characterCount = input
                 .toCharArray()
-                .filter { it != '-' }
+                .filter { it !in listOf(' ', '-') }
                 .map { it.toUpperCase() }
                 .groupBy { it }
         return characterCount.all { it.value.count() == 1 }
