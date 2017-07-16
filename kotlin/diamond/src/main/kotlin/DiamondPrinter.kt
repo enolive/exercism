0bbd9dd5518829646659f9d6436ae841a70360cf
Christoph Welcz <christoph@welcz.de>
2017-07-16 18:31:26 +0200

    some naming improvements

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,8 +1,9 @@
 class DiamondPrinter {
-    fun printToList(c: Char): List<String> {
-        return (('A'..c) + (c - 1 downTo 'A'))
-                .map { line(it, c) }
+    fun printToList(limit: Char): List<String> {
+        return range(limit).map { line(it, limit) }
     }
+
+    private fun range(limit: Char) = (('A'..limit) + (limit - 1 downTo 'A'))
 
     private fun line(current: Char, limit: Char): String {
         val numberOfEdgeSpaces = limit - current
@@ -13,10 +14,6 @@ class DiamondPrinter {
         }
 
         val middle = " ".repeat(numberOfMiddleSpaces)
-        return edges +
-                current +
-                middle +
-                current +
-                edges
+        return edges + current + middle + current + edges
     }
 }
