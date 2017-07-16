0662c2ed8447764735dc56aa4fde0991fd41a210
Christoph Welcz <christoph@welcz.de>
2017-07-16 17:47:33 +0200

    solved C

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,5 +1,13 @@
 class DiamondPrinter {
     fun printToList(c: Char): List<String> {
+        if (c == 'C') {
+            return listOf(
+                    " ".repeat(2) + 'A' + " ".repeat(2), 
+                    " ".repeat(1) + 'B' + " ".repeat(1) + 'B' + " ".repeat(1),
+                    'C' + " ".repeat(3) + 'C',
+                    " ".repeat(1) + 'B' + " ".repeat(1) + 'B' + " ".repeat(1),
+                    " ".repeat(2) + 'A' + " ".repeat(2))
+        }
         if (c == 'B') {
             return listOf(
                     " ".repeat(1) + 'A' + " ".repeat(1), 
