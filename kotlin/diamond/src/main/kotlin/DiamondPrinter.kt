0f48bb1f6face34978875c83bbcf9d505f92e7de
Christoph Welcz <christoph@welcz.de>
2017-07-16 19:50:32 +0200

    made the line creation much easier

--- kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
+++ kotlin/diamond/src/main/kotlin/DiamondPrinter.kt
@@ -1,24 +1,15 @@
 class DiamondPrinter {
-    fun printToList(limit: Char): List<String> {
-        val upperHalf = ('A'..limit).map { line(it, limit) }
+    fun printToList(max: Char): List<String> {
+        val upperHalf = ('A'..max).map { line(it, max) }
         return upperHalf + upperHalf.reversed().drop(1)
     }
 
-    private fun line(current: Char, limit: Char): String {
-        val lineHalf = edgeSpaces(limit, current) + current + middleSpacesHalf(current)
+    private fun line(current: Char, max: Char): String {
+        val lineHalf = edgeSpaces(current, max) + current + middleSpacesHalf(current)
         return lineHalf + lineHalf.reversed().drop(1)
     }
 
-    private fun edgeSpaces(limit: Char, current: Char) = " ".repeat(limit - current)
+    private fun edgeSpaces(current: Char, max: Char) = " ".repeat(max - current)
 
-    private fun middleSpacesHalf(current: Char): String {
-        val totalSpaces = 2 * (current - 'A') - 1
-        val middle = if (totalSpaces <= 0) {
-            ""
-        } else {
-            val addOneSpaceForLaterDrop = 1
-            " ".repeat(totalSpaces / 2 + addOneSpaceForLaterDrop)
-        }
-        return middle
-    }
+    private fun middleSpacesHalf(current: Char) = " ".repeat(current - 'A')
 }
