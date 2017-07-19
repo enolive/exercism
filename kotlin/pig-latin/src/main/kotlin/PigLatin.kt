62ca751b0113c121e9dc458ad7033090fdf962d9
Christoph Welcz <christoph@welcz.de>
2017-07-19 21:40:11 +0200

    first consonant test

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,5 +1,9 @@
 object PigLatin {
     fun translate(input: String): String {
+        if (input.startsWith("p")) {
+            return input.drop(1) + input.take(1) + "ay"
+        }
+        
         return input + "ay"
     }
 
