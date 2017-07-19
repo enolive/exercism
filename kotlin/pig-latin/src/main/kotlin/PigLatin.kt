b6613bf16d9e1ffead51af8acb9e9c693f5a92ae
Christoph Welcz <christoph@welcz.de>
2017-07-19 22:17:00 +0200

    all single words pass

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,12 +1,15 @@
 object PigLatin {
     fun translate(input: String): String {
-        val vowels = listOf("a", "e", "i", "o", "u")
-        val specialConsonants = listOf("ch", "qu", "sch", "squ", "th", "thr")
+        val treatLikeVowel = listOf("yt", "xr")
+        val vowels = listOf("a", "e", "i", "o", "u") + treatLikeVowel
+        val treatLikeConsonant = listOf("ch", "qu", "sch", "squ", "th", "thr")
         val consonants = ('a'..'z')
-                .map { it.toString() }
-                .minus(vowels)
-                .plus(specialConsonants)
+                .map { it.toString() } - vowels + treatLikeConsonant
 
+        if (vowels.any { input.startsWith(it) }) {
+            return input + "ay"
+        }
+        
         val startingConsonant = consonants
                 .filter { input.startsWith(it) }
                 .sortedBy { it.length }
