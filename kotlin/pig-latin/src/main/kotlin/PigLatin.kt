a582b90913dea47396f691fe429975fea5860e64
Christoph Welcz <christoph@welcz.de>
2017-07-19 22:08:35 +0200

    generalized consonants

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,12 +1,18 @@
 object PigLatin {
     fun translate(input: String): String {
-        val consonants = listOf("ch", "p", "qu", "squ", "th", "k", "x", "y")
+        val vowels = listOf("a", "e", "i", "o", "u")
+        val specialConsonants = listOf("ch", "qu", "squ", "th")
+        val consonants = ('a'..'z').map { it.toString() }.minus(vowels).plus(specialConsonants)
 
-        val foundConsonant = consonants.firstOrNull { input.startsWith(it) }
+        val foundConsonant = consonants
+                .filter { input.startsWith(it) }
+                .sortedBy { it.length }
+                .reversed()
+                .firstOrNull()
         if (foundConsonant != null) {
             return input.drop(foundConsonant.length) + input.take(foundConsonant.length) + "ay"
         }
-        
+
         return input + "ay"
     }
 
