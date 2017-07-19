a34d2066abd634b044965f4a68c94c5610b04b6d
Christoph Welcz <christoph@welcz.de>
2017-07-19 21:59:59 +0200

    even more tests

--- kotlin/pig-latin/src/main/kotlin/PigLatin.kt
+++ kotlin/pig-latin/src/main/kotlin/PigLatin.kt
@@ -1,10 +1,10 @@
 object PigLatin {
     fun translate(input: String): String {
-        val consonants = listOf("ch", "p", "qu", "k", "x", "y")
+        val consonants = listOf("ch", "p", "qu", "squ", "th", "k", "x", "y")
 
-        val any = consonants.firstOrNull { input.startsWith(it) }
-        if (any != null) {
-            return input.drop(any.length) + input.take(any.length) + "ay"
+        val foundConsonant = consonants.firstOrNull { input.startsWith(it) }
+        if (foundConsonant != null) {
+            return input.drop(foundConsonant.length) + input.take(foundConsonant.length) + "ay"
         }
         
         return input + "ay"
