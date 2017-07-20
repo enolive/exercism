667d2252b217d0b5771d88fc760c1da41e109209
Christoph Welcz <christoph@welcz.de>
2017-07-21 00:38:03 +0200

    removed some duplication

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,22 +1,28 @@
 class PhoneNumber(rawNumber: String) {
     val number: String
     val areaCode: String
+    private val countryCode = "1"
 
     init {
         val cleanNumber = rawNumber.filter { it.isDigit() }
         require(when {
             cleanNumber.length < 10 -> false
-            cleanNumber.length == 10 && cleanNumber.startsWith("1") -> false
-            cleanNumber.length == 11 && !cleanNumber.startsWith("1") -> false
+            cleanNumber.length == 10 && cleanNumber.startsWith(countryCode) -> false
+            cleanNumber.hasCountryCode() && !cleanNumber.startsWith(countryCode) -> false
             else -> true
         })
 
-        number = if (cleanNumber.length == 11) {
-            cleanNumber.drop(1)
-        } else {
-            cleanNumber
-        }
+        number = dropCountryCode(cleanNumber)
         areaCode = rawNumber.take(3)
     }
+
+    private fun dropCountryCode(cleanNumber: String): String {
+        return when {
+            cleanNumber.hasCountryCode() -> cleanNumber.drop(countryCode.length)
+            else -> cleanNumber
+        }
+    }
+
+    private fun String.hasCountryCode() = length == 11
 
 }
