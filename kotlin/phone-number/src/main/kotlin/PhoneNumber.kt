37ebf29920cb8f0739d0aa2aa46f84fc5fc1f246
Christoph Welcz <christoph@welcz.de>
2017-07-21 01:22:51 +0200

    removed some unnecessary generalization

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,6 +1,4 @@
 class PhoneNumber(rawNumber: String) {
-    private val countryCode = "1"
-
     val number: String
     val areaCode: String
 
@@ -16,7 +14,7 @@ class PhoneNumber(rawNumber: String) {
     private fun format(rawNumber: String): String {
         return rawNumber.filter { it.isDigit() }
                 .let {
-                    if (it.startsWith(countryCode)) it.drop(countryCode.length)
+                    if (it.startsWith("1")) it.drop(1)
                     else it
                 }
     }
