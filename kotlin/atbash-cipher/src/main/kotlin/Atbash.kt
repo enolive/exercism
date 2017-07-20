2d05ebfabe83a2bfc593721ff91721121fee9c6e
Christoph Welcz <christoph@welcz.de>
2017-07-20 19:49:44 +0200

    some refactorings

--- kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
+++ kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
@@ -4,17 +4,10 @@ object Atbash {
     }
 
     fun encode(input: String): String {
-        val chunkSize = 5
         val sanitizedInput = input.filter { it.isLetterOrDigit() }.toLowerCase()
-        return sanitizedInput.splitIntoChunksOf(chunkSize)
+        return sanitizedInput.splitIntoChunksOf(5)
                 .map { encodeChunk(it) }
                 .joinToString(" ")
-    }
-
-    private fun String.splitIntoChunksOf(chunkSize: Int): List<String> {
-        val numberOfChunks = Math.ceil(length.toDouble() / chunkSize).toInt()
-        val chunkify = chunkify(numberOfChunks, chunkSize)
-        return chunkify
     }
 
     private fun encodeChunk(chunk: String): String {
@@ -26,13 +19,18 @@ object Atbash {
 
     private fun encode(c: Char) = if (!c.isLetter()) c else 'z' - (c - 'a')
 
-    private fun String.chunkify(numberOfChunks: Int, chunkSize: Int) =
-            (0 until numberOfChunks).map { getChunk(it, chunkSize) }
+    private fun String.splitIntoChunksOf(chunkSize: Int): List<String> {
+        val numberOfChunks = Math.ceil(length.toDouble() / chunkSize).toInt()
+        return (0 until numberOfChunks).map { getChunk(it, chunkSize) }
+    }
 
-    fun String.getChunk(indexOfChunk: Int, chunkSize: Int): String {
+    private fun String.getChunk(indexOfChunk: Int, chunkSize: Int): String {
         val startIndex = indexOfChunk * chunkSize
         val endIndex = startIndex + chunkSize
-        return if (endIndex >= length) substring(startIndex) else substring(startIndex, endIndex)
+        return substringOrRest(endIndex, startIndex)
     }
+
+    private fun String.substringOrRest(endIndex: Int, startIndex: Int) 
+            = if (endIndex >= length) substring(startIndex) else substring(startIndex, endIndex)
 }
 
