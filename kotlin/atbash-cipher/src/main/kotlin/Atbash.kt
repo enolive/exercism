180a38df17bce7a5728da39e8023ac15726d9337
Christoph Welcz <christoph@welcz.de>
2017-07-20 17:52:26 +0200

    finally understood how substring in kotlin works, argh!

--- kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
+++ kotlin/atbash-cipher/src/main/kotlin/Atbash.kt
@@ -4,11 +4,28 @@ object Atbash {
     }
 
     fun encode(input: String): String {
-        return input.toCharArray()
+        val chunkSize = 5
+        val sanitizedInput = input.toLowerCase()
+        val numberOfChunks = sanitizedInput.length / chunkSize
+        return (0..numberOfChunks)
+                .map { sanitizedInput.getChunk(it, chunkSize) }
+                .map { encodeChunk(it) }
+                .joinToString(" ")
+    }
+
+    private fun encodeChunk(chunk: String): String {
+        return chunk
+                .toCharArray()
                 .map { encode(it) }
                 .joinToString("")
     }
 
     private fun encode(c: Char) = 'z' - (c - 'a')
 
+    fun String.getChunk(indexOfChunk: Int, chunkSize: Int): String {
+        val startIndex = indexOfChunk * chunkSize
+        val endIndex = startIndex + chunkSize
+        return if (endIndex >= length) substring(startIndex) else substring(startIndex, endIndex)
+    }
 }
+
