0373673e2fc6bc3c12fb6774c5b8bf5d6b6f5971
Christoph Welcz <christoph@welcz.de>
2017-07-21 01:21:33 +0200

    it is easier to first drop the country code and then validate everything

--- kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
+++ kotlin/phone-number/src/main/kotlin/PhoneNumber.kt
@@ -1,44 +1,24 @@
 class PhoneNumber(rawNumber: String) {
+    private val countryCode = "1"
+
     val number: String
     val areaCode: String
-    private val countryCode = "1"
-    private val stringRepresentation: String
 
     init {
-        val cleanNumber = rawNumber.filter { it.isDigit() }
-        require(isValid(cleanNumber))
-
-        number = dropCountryCode(cleanNumber)
+        number = format(rawNumber)
         areaCode = number.take(3)
-
-        stringRepresentation = makeString()
+        require(number.length == 10)
     }
 
-    private fun makeString(): String {
-        val strippedNumber = number.drop(areaCode.length)
-        return "($areaCode) ${strippedNumber.take(3)}-${strippedNumber.drop(3)}"
+    override fun toString(): String = 
+            "($areaCode) ${number.substring(3, 6)}-${number.substring(6, 10)}"
+
+    private fun format(rawNumber: String): String {
+        return rawNumber.filter { it.isDigit() }
+                .let {
+                    if (it.startsWith(countryCode)) it.drop(countryCode.length)
+                    else it
+                }
     }
-
-    override fun toString(): String {
-        return stringRepresentation
-    }
-
-    private fun isValid(number: String): Boolean {
-        return when {
-            number.length < 10 -> false
-            hasNoCountryCode(number) && number.startsWith(this.countryCode) -> false
-            hasCountryCode(number) && !number.startsWith(this.countryCode) -> false
-            else -> true
-        }
-    }
-
-    private fun dropCountryCode(number: String): String =
-            when {
-                hasCountryCode(number) -> number.drop(this.countryCode.length)
-                else -> number
-            }
-
-    private fun hasNoCountryCode(number: String) = number.length == 10
-    private fun hasCountryCode(number: String) = number.length == 11
 
 }
