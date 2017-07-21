f8053d41226a7a69fcbab4ccf130b96559e0024d
Christoph Welcz <christoph@welcz.de>
2017-07-21 14:12:22 +0200

    moved implementation

---
+++ kotlin/nucleotide-count/src/main/kotlin/DNA.kt
@@ -0,0 +1,13 @@
+class DNA(value: String) {
+    fun count(nucleotide: Char): Int {
+        return 0
+    }
+
+    val nucleotideCounts: Map<Char, Int> = mapOf(
+            'A' to 0,
+            'C' to 0,
+            'G' to 0,
+            'T' to 0
+    )
+
+}
--- kotlin/nucleotide-count/src/test/kotlin/NucleotideTest.kt
+++ kotlin/nucleotide-count/src/test/kotlin/NucleotideTest.kt
@@ -84,16 +84,3 @@ class NucleotideTest {
     }
 }
 
-class DNA(value: String) {
-    fun count(nucleotide: Char): Int {
-        return 0
-    }
-
-    val nucleotideCounts: Map<Char, Int> = mapOf(
-            'A' to 0,
-            'C' to 0,
-            'G' to 0,
-            'T' to 0
-    )
-
-}
