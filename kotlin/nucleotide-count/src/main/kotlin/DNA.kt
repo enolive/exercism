b0f5be66dcd3f0980c9aedf5a5b5f40dc58df422
Christoph Welcz <christoph@welcz.de>
2017-07-21 14:33:52 +0200

    optimized init

--- kotlin/nucleotide-count/src/main/kotlin/DNA.kt
+++ kotlin/nucleotide-count/src/main/kotlin/DNA.kt
@@ -1,13 +1,14 @@
 class DNA(private val value: String) {
-    private val nucleotides = listOf('A', 'C', 'G', 'T')
+
+    val nucleotideCounts: Map<Char, Int>
+    private val nucleotides: List<Char> = listOf('A', 'C', 'G', 'T')
 
     init {
         require(value.all { it.isValid() })
+        nucleotideCounts = nucleotides
+                .associateBy({ it }, { nucleotide -> value.count { it == nucleotide } })
     }
-
-    val nucleotideCounts: Map<Char, Int> = nucleotides
-            .associateBy({ it }, { nucleotide -> value.count { it == nucleotide } })
-
+    
     fun count(nucleotide: Char): Int {
         require(nucleotide.isValid())
         return nucleotideCounts.getValue(nucleotide)
