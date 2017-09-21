492912e22c887380633402632a8df3685033e154
Christoph Welcz <christoph@welcz.de>
2017-09-21 09:20:48 +0200

    second test

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,8 +1,11 @@
 export default class Pangram {
-    constructor(s: string) {
+    constructor(private readonly s: string) {
     }
 
     isPangram() {
+        if (this.s.length > 0) {
+            return true
+        }
         return false
     }
 }
