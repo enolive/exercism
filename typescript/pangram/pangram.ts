d21cc6b5b46d7e02e18dc2a6444d3e2d339ef1ca
Christoph Welcz <christoph@welcz.de>
2017-09-22 21:45:26 +0200

    using lodash from a better character range

--- typescript/pangram/pangram.ts
+++ typescript/pangram/pangram.ts
@@ -1,5 +1,7 @@
+import _ = require('lodash')
+
 export default class Pangram {
-    private alphabet = new Set<string>('abcdefghijklmnopqrstuvwxyz'.split(''))
+    private alphabet = new Set<string>(this.characterRange('a', 'z'))
 
     constructor(private readonly sentence: string) {
     }
@@ -10,6 +12,11 @@ export default class Pangram {
             .split('')
             .filter(c => this.isLetter(c)))
         return presentCharacters.size === this.alphabet.size
+    }
+
+    private characterRange(from: string, to: string) {
+        return _.range(from.charCodeAt(0), to.charCodeAt(0), 1)
+            .map(c => String.fromCharCode(c))
     }
 
     private isLetter(character: string) {
