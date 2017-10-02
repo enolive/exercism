26154fb9e15b298eb816cb657c811d7239424266
Christoph Welcz <christoph@welcz.de>
2017-10-02 19:53:56 +0200

    made more test pass

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,11 +1,14 @@
 export default class GradeSchool {
-    private _map = new Map<string, string[]>()
+    private map = new Map<string, string[]>()
 
     studentRoster() {
-        return this._map
+        return this.map
     }
 
     addStudent(name: string, grade: number) {
-        this._map.set(grade.toString(), [name])
+        const key = grade.toString()
+        const entry = this.map.get(key) || []
+        entry.push(name)
+        this.map.set(key, entry)
     }
 }
