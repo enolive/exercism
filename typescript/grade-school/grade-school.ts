2222afbc018dee276809886553a8087167137d0e
Christoph Welcz <christoph@welcz.de>
2017-10-04 17:14:18 +0200

    all tests pass

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -1,26 +1,42 @@
 export default class GradeSchool {
     private map = new Map<string, string[]>()
 
+    private static copyNames(originalNames: string[]) {
+        return [...originalNames]
+    }
+
+    private static copyEntry(entry: [string, string[]]) {
+        const [key, names] = entry
+        return [key, GradeSchool.copyNames(names)]
+    }
+
+    private static key(grade: number) {
+        return grade.toString()
+    }
+
+    private static copyEntries(entries: IterableIterator<[string, string[]]>) {
+        return [...entries]
+            .map((entry) => GradeSchool.copyEntry(entry))
+            .sort() as Array<[string, string[]]>
+    }
+
     studentRoster() {
-        return new Map([...this.map.entries()].sort())
+        return new Map(GradeSchool.copyEntries(this.map.entries()))
     }
 
     addStudent(name: string, grade: number) {
-        const entry = this.entry(grade)
+        const entry = this.names(grade)
         entry.push(name)
         entry.sort()
-        this.map.set(this.key(grade), entry)
+        this.map.set(GradeSchool.key(grade), entry)
     }
 
     studentsInGrade(grade: number) {
-        return this.entry(grade)
+        const originalNames = this.names(grade)
+        return GradeSchool.copyNames(originalNames)
     }
 
-    private entry(grade: number) {
-        return this.map.get(this.key(grade)) || []
-    }
-
-    private key(grade: number) {
-        return grade.toString()
+    private names(grade: number) {
+        return this.map.get(GradeSchool.key(grade)) || []
     }
 }
