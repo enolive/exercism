fd74385c41b64a939c0a6d947af3ba47f80825cf
Christoph Welcz <christoph@welcz.de>
2017-10-18 21:43:31 +0200

    optimized isPrime a bit

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash"
+
 class Result {
     constructor(private readonly limit: number, readonly primes: number[] = []) {
     }
@@ -5,19 +7,26 @@ class Result {
     next(prime: number): Result {
         return new Result(this.limit / prime, this.primes.concat(prime))
     }
-    
+
     isAtEnd() {
         return this.limit === 1
     }
 }
 
-function letThereBePrimes(limit: number) {
-    return [2, 3, 5].filter((p) => limit % p === 0)
+export function isPrime(candidate: number): boolean {
+    const upperLimit = Math.floor(Math.sqrt(candidate))
+    const numbers = _.range(2, upperLimit + 1)
+    return !numbers.some((n) => candidate % n === 0)
+}
+
+export function letThereBePrimes(limit: number) {
+    const numbers = _.range(2, limit + 1)
+    return numbers.filter((primeCandidate) => isPrime(primeCandidate))
 }
 
 export default function calculatePrimeFactors(limit: number) {
     const result = new Result(limit)
-    const primes = letThereBePrimes(limit)
+    const primes = letThereBePrimes(limit).filter((p) => limit % p === 0)
 
     function calculateTailRec(result: Result): Result {
         if (result.isAtEnd()) {
