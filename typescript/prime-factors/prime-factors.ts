8947b5cbe24ea936af29dfe1ce5eb68eb84ac48b
Christoph Welcz <christoph@welcz.de>
2017-10-18 19:41:53 +0200

    starting to looking good

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -1,20 +1,14 @@
 export default function calculatePrimeFactors(limit: number) {
     let result: number[] = []
-    if (limit % 2 === 0) {
-        result = result.concat(2)
-        limit /= 2
-    }
-    if (limit % 3 === 0) {
-        result = result.concat(3)
-        limit /= 3
-    }
-    if (limit === 4) {
-        result = [2].concat(result)
-        limit /= 2
-    }
-    if (limit === 2) {
-        result = [2].concat(result)
-        limit /= 2
+    while (limit > 1) {
+        if (limit % 2 === 0) {
+            result = result.concat(2)
+            limit /= 2
+        }
+        if (limit % 3 === 0) {
+            result = result.concat(3)
+            limit /= 3
+        }
     }
     return result
 }
