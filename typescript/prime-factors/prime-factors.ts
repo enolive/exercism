ff08fae69cacbab6fb380f27ab903bfc32115850
Christoph Welcz <christoph@welcz.de>
2017-10-18 20:33:12 +0200

    introduced tail recursion to remove this nasty while

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -19,9 +19,18 @@ export default function calculatePrimeFactors(limit: number) {
     let result = new Result(limit)
     const primes = [2, 3].filter((p) => result.isDivisibleBy(p))
 
-    while (!result.isAtEnd()) {
-        result = primes.reduce((acc, prime) => acc.next(prime), result)
+    function calculateTailRec(result: Result): Result {
+        if (result.isAtEnd()) {
+            return result
+        }
+        return calculateTailRec(primes.reduce((acc, prime) => acc.next(prime), result))
     }
-    
+    return calculateTailRec(result).primes
+
+    while (!result.isAtEnd()) {
+        const newResult = primes.reduce((acc, prime) => acc.next(prime), result)
+        result = newResult
+    }
+
     return result.primes
 }
