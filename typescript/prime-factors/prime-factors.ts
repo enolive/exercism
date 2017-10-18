9d6401640b505a0ff5166b88c6cedf72babaad01
Christoph Welcz <christoph@welcz.de>
2017-10-18 20:21:18 +0200

    removed rest of the feature envy

--- typescript/prime-factors/prime-factors.ts
+++ typescript/prime-factors/prime-factors.ts
@@ -5,18 +5,24 @@ class Result {
     next(prime: number): Result {
         return new Result(this.limit / prime, this.primes.concat(prime))
     }
+
+    isDivisibleBy(prime: number) {
+        return this.limit % prime === 0
+    }
+
+    isAtEnd() {
+        return this.limit === 1
+    }
 }
 
 export default function calculatePrimeFactors(limit: number) {
     let result = new Result(limit)
-    const primes = [2, 3]
+    const primes = [2, 3].filter((p) => result.isDivisibleBy(p))
 
-    while (result.limit > 1) {
-        primes
-            .filter((p) => result.limit % p === 0)
-            .forEach((p) => {
-                result = result.next(p)
-            })
+    while (!result.isAtEnd()) {
+        primes.forEach((p) => {
+            result = result.next(p)
+        })
     }
     return result.primes
 }
