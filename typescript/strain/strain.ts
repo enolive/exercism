d548abb833d2e4cb0e677f69b0e48879d8239e07
Christoph Welcz <christoph@welcz.de>
2018-01-11 01:13:06 +0100

    solved discard

--- typescript/strain/strain.ts
+++ typescript/strain/strain.ts
@@ -8,6 +8,14 @@ export function keep<Type>(array: Type[], predicate: (item: Type) => boolean): T
     }
     return newArray
 }
-export function discard<Type>(array: Type[], predicate: (item: Type) => boolean) {
-    return array
+
+export function discard<Type>(array: Type[], predicate: (item: Type) => boolean): Type[] {
+    const newArray = []
+    for (const item of array) {
+        if (predicate(item)) {
+            continue
+        }
+        newArray.push(item)
+    }
+    return newArray
 }
