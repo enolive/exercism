a77fb47df6e60952b5ecfa4ae30f7191f839d08d
Christoph Welcz <christoph@welcz.de>
2018-01-11 01:04:00 +0100

    initial setup

---
+++ typescript/strain/strain.ts
@@ -0,0 +1,6 @@
+export function keep<Type>(array: Type[], predicate: (item: Type) => boolean) {
+    return []
+}
+export function discard<Type>(array: Type[], predicate: (item: Type) => boolean) {
+    return array
+}
