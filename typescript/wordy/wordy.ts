95807656fa46e16c76da0dbf140362b0f83113a1
Christoph Welcz <christoph@welcz.de>
2018-01-14 16:03:19 +0100

    some cleanup

--- typescript/wordy/wordy.ts
+++ typescript/wordy/wordy.ts
@@ -1,3 +1,12 @@
+export class ArgumentError implements Error {
+    name: string = 'ArgumentError'
+    message: string
+
+    constructor(message: string) {
+        this.message = message
+    }
+}
+
 export class WordProblem {
     private question: string
 
@@ -6,15 +15,25 @@ export class WordProblem {
     }
 
     answer() {
-        const match = this.question.match(/What is ([^ ]+) plus ([^ ]+)\?/)
-        if (match) {
-            const firstNumber = parseInt(match[1], 10)
-            const secondNumber = parseInt(match[2], 10)
-            return firstNumber + secondNumber
+        const match = this.question.match(/What is ([^ ]+) ((plus) ([^ ]+))\?/)
+        if (!match) {
+            return 42
         }
-        if (this.question === 'What is 53 plus 2?') {
-            return 55
+        const firstNumber = this.toDecimal(match[1])
+        const secondNumber = this.toDecimal(match[4])
+        return this.getOperation(match[3])(firstNumber, secondNumber)
+    }
+
+    private toDecimal(value: string) {
+        return parseInt(value, 10)
+    }
+
+    private getOperation(operationCode: string) {
+        switch (operationCode) {
+            case "plus":
+                return (a: number, b: number) => a + b
+            default:
+                throw new ArgumentError(`unknown operation code ${operationCode}`)
         }
-        return 2
     }
 }
