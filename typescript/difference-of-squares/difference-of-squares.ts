d9f026566c45be44e5de2cc9fb0f0b8ab3f78b6f
Christoph Welcz <christoph@welcz.de>
2018-01-14 23:40:08 +0100

    using the magic powers of math!

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -1,15 +1,21 @@
-import * as _ from "lodash"
-
 export default class DifferenceOfSquares {
     readonly squareOfSums: number
     readonly sumOfSquares: number
     readonly difference: number
-    private sum = (a: number, b: number): number => a + b
-    private numbersUpTo = (end: number) => _.range(1, end + 1)
 
     constructor(end: number) {
-        this.squareOfSums = this.numbersUpTo(end).reduce(this.sum) ** 2
-        this.sumOfSquares = this.numbersUpTo(end).map((value) => value ** 2).reduce(this.sum)
+        this.squareOfSums = DifferenceOfSquares.arithmeticSum(end) ** 2
+        this.sumOfSquares = DifferenceOfSquares.squarePyramidalNumber(end)
         this.difference = this.squareOfSums - this.sumOfSquares
+    }
+
+    private static arithmeticSum(end: number) {
+        // see https://en.wikipedia.org/wiki/Arithmetic_progression#Sum
+        return (1 + end) * end / 2
+    }
+
+    private static squarePyramidalNumber(end: number) {
+        // see https://en.wikipedia.org/wiki/Square_pyramidal_number
+        return end * (end + 1) * (2 * end + 1) / 6
     }
 }
