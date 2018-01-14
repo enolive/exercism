abc394582066246c753c98e83c77d581fab3fbaa
Christoph Welcz <christoph@welcz.de>
2018-01-14 19:48:01 +0100

    solved multiple operations

--- typescript/wordy/wordy.ts
+++ typescript/wordy/wordy.ts
@@ -8,38 +8,67 @@ export class ArgumentError implements Error {
 }
 
 export class WordProblem {
-    private question: string
     private operations = [
-        {word: 'plus', compute: (a: number, b: number) => a + b},
-        {word: 'minus', compute: (a: number, b: number) => a - b},
-        {word: 'multiplied by', compute: (a: number, b: number) => a * b},
-        {word: 'divided by', compute: (a: number, b: number) => a / b},
+        {code: 'plus', func: (a: number, b: number) => a + b},
+        {code: 'minus', func: (a: number, b: number) => a - b},
+        {code: 'multiplied by', func: (a: number, b: number) => a * b},
+        {code: 'divided by', func: (a: number, b: number) => a / b},
     ]
+
+    private question: string
 
     constructor(question: string) {
         this.question = question
     }
 
     answer() {
-        const allowedOperations = this.operations.map((o) => o.word).join('|')
-        const regExp = new RegExp(`What is (-?\\d+) ((${allowedOperations}) (-?\\d+))\\?`)
-        const match = this.question.match(regExp)
-        if (!match) {
-            throw new ArgumentError(`I don't understand the given question ${this.question}`)
-        }
-        const firstNumber = this.toDecimal(match[1])
-        const secondNumber = this.toDecimal(match[4])
-        return this.getOperation(match[3])(firstNumber, secondNumber)
+        return this.processOperations(this.determineFirstNumber())
     }
 
-    private toDecimal(value: string) {
+    private secondTerm() {
+        const allowedOperations = this.operations
+            .map((o) => o.code)
+            .join('|')
+        return `((${allowedOperations}) (-?\\d+))`
+    }
+
+    private processOperations(initialValue: number) {
+        const operationPattern = new RegExp(this.secondTerm(), 'g')
+        let result = initialValue
+        let matchOperation: RegExpExecArray | null
+        do {
+            matchOperation = operationPattern.exec(this.question)
+            result = this.processOperation(matchOperation, result)
+        } while (matchOperation)
+        return result
+    }
+
+    private processOperation(matchOperation: RegExpExecArray | null, result: number) {
+        if (matchOperation) {
+            const operation = this.getOperation(matchOperation[2])
+            const secondNumber = WordProblem.toDecimal(matchOperation[3])
+            result = operation(result, secondNumber)
+        }
+        return result
+    }
+
+    private determineFirstNumber() {
+        const wholePattern = new RegExp(`What is (-?\\d+)( ${this.secondTerm()})+\\?`)
+        const matchWhole = wholePattern.exec(this.question)
+        if (!matchWhole) {
+            throw new ArgumentError(`I don't understand the given question ${this.question}`)
+        }
+        return WordProblem.toDecimal(matchWhole[1])
+    }
+
+    private static toDecimal(value: string) {
         return parseInt(value, 10)
     }
 
     private getOperation(operationCode: string) {
         const [operation] = this.operations
-            .filter((o) => o.word === operationCode)
-            .map((o) => o.compute)
+            .filter((o) => o.code === operationCode)
+            .map((o) => o.func)
         return operation
     }
 }
