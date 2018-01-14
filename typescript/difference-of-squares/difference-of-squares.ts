6910e9ba151c88c04fe09a6158efa9cff9cdadef
Christoph Welcz <christoph@welcz.de>
2018-01-14 23:20:04 +0100

    solved up to 5

--- typescript/difference-of-squares/difference-of-squares.ts
+++ typescript/difference-of-squares/difference-of-squares.ts
@@ -2,9 +2,14 @@ import * as _ from "lodash"
 
 export default class DifferenceOfSquares {
     readonly squareOfSums: number
+    readonly sumOfSquares: number
+    readonly difference: number
     private sum = (a: number, b: number): number => a + b
+    private numbersUpTo = (end: number) => _.range(1, end + 1)
 
     constructor(end: number) {
-        this.squareOfSums = _.range(1, end + 1).reduce(this.sum) ** 2
+        this.squareOfSums = this.numbersUpTo(end).reduce(this.sum) ** 2
+        this.sumOfSquares = this.numbersUpTo(end).map((value) => value ** 2).reduce(this.sum)
+        this.difference = this.squareOfSums - this.sumOfSquares
     }
 }
