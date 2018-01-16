6b746b807bfac6ad22050762b7cd78a2bf7fa776
Christoph Welcz <christoph@welcz.de>
2018-01-16 17:57:10 +0100

    extracted filling the result

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,9 +1,15 @@
 export default function transform(old: { [p: string]: string[] }) {
+    const result: {[key: string]: number} = {}
     const keys = Object.keys(old)
     const key = keys[0]
+    processKey(old, key, result)
+    return result
+}
+const processKey = (old: { [p: string]: string[] }, key: string, result: { [p: string]: number }) => {
     const values = old[key]
     const value = values[0].toLowerCase()
-    const result: {[key: string]: number} = {}
+    fill(result, value, key)
+}
+const fill = (result: { [p: string]: number }, value: string, key: string) => {
     result[value] = +key
-    return result
 }
