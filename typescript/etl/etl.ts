5a5ba7fb38cd41b500a26e058bd561995ce2b683
Christoph Welcz <christoph@welcz.de>
2018-01-16 21:02:26 +0100

    KISS

--- typescript/etl/etl.ts
+++ typescript/etl/etl.ts
@@ -1,24 +1,11 @@
 type OldStructure = { [key: string]: string[] }
 type NewStructure = { [p: string]: number }
 
-class OldEntry {
-    private readonly key: number
-
-    constructor(key: string, private values: string[]) {
-        this.key = +key
-    }
-
-    forEachValue(callback: (key: number, value: string) => void) {
-        this.values
-            .map((value) => value.toLowerCase())
-            .forEach((value) => callback(this.key, value))
-    }
-}
-
 export default (old: OldStructure) => {
     const result: NewStructure = {}
     Object.entries(old)
-        .map(([key, values]) => new OldEntry(key, values))
-        .forEach((entry) => entry.forEachValue((key, value) => result[value] = key))
+        .forEach(([key, values]) =>
+            values.forEach((value) =>
+                result[value.toLowerCase()] = +key))
     return result
 }
