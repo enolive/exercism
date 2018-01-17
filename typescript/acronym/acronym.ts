2a795b9b06a03d676799995a4e7fabaef2c5d866
Christoph Welcz <christoph@welcz.de>
2018-01-17 12:58:27 +0100

    now can detect acronyms at the middle of a word

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,7 +1,8 @@
 export default class Acronym {
     static parse(phrase: string): string {
         return phrase
-            .split(' ')
+            .split('')
+            .filter((letter, index) => (letter.match(/[A-Z]/) || phrase.charAt(index - 1) === ' '))
             .map((word) => word.charAt(0).toUpperCase())
             .join('')
     }
