0daece3fda6a692f823b2978a903c836d9681c08
Christoph Welcz <christoph@welcz.de>
2018-01-17 12:49:00 +0100

    solved second test

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,5 +1,5 @@
 export default class Acronym {
-  static parse(phrase: string): string {
-    return 'PNG'
-  }
+    static parse(phrase: string): string {
+        return phrase.split(' ').map((word) => word.charAt(0).toUpperCase()).join('')
+    }
 }
