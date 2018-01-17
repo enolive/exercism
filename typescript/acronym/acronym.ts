291e955a7f22e813802be25f6d4af1097a34db1e
Christoph Welcz <christoph@welcz.de>
2018-01-17 13:00:30 +0100

    some cleanup

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -2,8 +2,12 @@ export default class Acronym {
     static parse(phrase: string): string {
         return phrase
             .split('')
-            .filter((letter, index) => (letter.match(/[A-Z]/) || phrase.charAt(index - 1) === ' '))
-            .map((word) => word.charAt(0).toUpperCase())
+            .filter((letter, index) => this.startsNewWord(phrase, letter, index))
+            .map((word) => word.toUpperCase())
             .join('')
+    }
+
+    private static startsNewWord(phrase: string, letter: string, index: number) {
+        return letter.match(/[A-Z]/) || phrase.charAt(index - 1) === ' '
     }
 }
