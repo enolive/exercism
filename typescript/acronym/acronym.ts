a68bbde41849d9cce8a21c8df9d0a2ead64338b1
Christoph Welcz <christoph@welcz.de>
2018-01-18 00:07:37 +0100

    one expression for function

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,24 +1,47 @@
 export default class Acronym {
-    static parse(phrase: string): string {
+    private static ruleChain: [(phrase: string, index: number) => boolean] = [
+        Acronym.isStartOfSentence,
+        Acronym.isUpperCaseFollowingLowerCase,
+        Acronym.isFormerWhitespaceOrHyphen
+    ]
+
+    static parse = (phrase: string): string => {
         return phrase
             .split('')
-            .filter((_, index) => this.startsNewWord(phrase, index))
+            .filter((_, index) => Acronym.startsNewWord(phrase, index))
             .map((word) => word.toUpperCase())
             .join('')
     }
 
     private static startsNewWord(phrase: string, index: number) {
-        if (index === 0) {
-            return true
-        }
-        const formerLetter = phrase.charAt(index - 1)
-        const currentLetter = phrase.charAt(index)
-        if (currentLetter.match(/[A-Z]/) && (formerLetter.match(/[a-z]/))) {
-            return true
-        }
-        if (formerLetter === ' ' || formerLetter === '-') {
-            return true
-        }
-        return false
+        return Acronym.ruleChain.some((rule) => rule(phrase, index))
+    }
+
+    private static isStartOfSentence(_: string, index: number): boolean {
+        return index === 0
+    }
+
+    private static isUpperCaseFollowingLowerCase(phrase: string, index: number): boolean {
+        return Acronym.isCurrentUpperCase(phrase, index) && Acronym.isFormerLowerCase(phrase, index)
+    }
+
+    private static isFormerWhitespaceOrHyphen(phrase: string, index: number): boolean {
+        return [' ', '-'].some((c) => Acronym.formerLetter(phrase, index) === c)
+    }
+
+    private static isFormerLowerCase(phrase: string, index: number): boolean {
+        return Acronym.formerLetter(phrase, index).match(/[a-z]/) !== null
+    }
+
+    private static isCurrentUpperCase(phrase: string, index: number): boolean {
+        return Acronym.currentLetter(phrase, index).match(/[A-Z]/) !== null
+    }
+
+    private static currentLetter(phrase: string, index: number): string {
+        return phrase.charAt(index)
+    }
+
+    private static formerLetter(phrase: string, index: number): string {
+        return phrase.charAt(index - 1)
     }
 }
