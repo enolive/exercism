a2606ba12ba5e999d47592d9727158c0a28764da
Christoph Welcz <christoph@welcz.de>
2018-01-18 18:07:17 +0100

    much easier solution using Regexp

--- typescript/acronym/acronym.ts
+++ typescript/acronym/acronym.ts
@@ -1,50 +1,9 @@
 export default class Acronym {
-    private static startNewWordRules: [(phrase: string, index: number) => boolean] = [
-        Acronym.isStartOfSentence,
-        Acronym.isUpperCaseFollowingLowerCase,
-        Acronym.isFormerOneOf(' ', '-')
-    ]
-
-    static parse(phrase: string): string {
-        return phrase
-            .split('')
-            .filter((_, index) => Acronym.startsNewWord(phrase, index))
-            .map((word) => word.toUpperCase())
+    static parse = (phrase: string): string =>
+        Acronym.wordsFrom(phrase)
+            .map((word) => word[0].toUpperCase())
             .join('')
-    }
 
-    private static startsNewWord(phrase: string, index: number) {
-        return Acronym.startNewWordRules.some((rule) => rule(phrase, index))
-    }
-
-    // noinspection JSUnusedLocalSymbols
-    private static isStartOfSentence(_: string, index: number): boolean {
-        return index === 0
-    }
-
-    private static isUpperCaseFollowingLowerCase(phrase: string, index: number): boolean {
-        return Acronym.isCurrentUpperCase(phrase, index) && Acronym.isFormerLowerCase(phrase, index)
-    }
-
-    private static isFormerOneOf(...characters: string[]) {
-        return (phrase: string, index: number): boolean => {
-            return characters.some((c) => Acronym.formerLetter(phrase, index) === c)
-        }
-    }
-
-    private static isFormerLowerCase(phrase: string, index: number): boolean {
-        return Acronym.formerLetter(phrase, index).match(/[a-z]/) !== null
-    }
-
-    private static isCurrentUpperCase(phrase: string, index: number): boolean {
-        return Acronym.currentLetter(phrase, index).match(/[A-Z]/) !== null
-    }
-
-    private static currentLetter(phrase: string, index: number): string {
-        return phrase.charAt(index)
-    }
-
-    private static formerLetter(phrase: string, index: number): string {
-        return phrase.charAt(index - 1)
-    }
+    private static wordsFrom = (phrase: string) =>
+        (phrase.match(/[A-Z]+[a-z]*|[a-z]+/g) || [])
 }
