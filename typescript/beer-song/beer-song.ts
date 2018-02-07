78f9921ebae3a68966b5288b80d158939277e39f
Christoph Welcz <christoph@welcz.de>
2018-02-07 15:39:45 +0100

    initial setup with one passing test

---
+++ typescript/beer-song/beer-song.ts
@@ -0,0 +1,8 @@
+export default class Beer {
+
+    static verse(verseNumber: number) {
+        return `8 bottles of beer on the wall, 8 bottles of beer.
+Take one down and pass it around, 7 bottles of beer on the wall.
+`
+    }
+}
