f9afa8bc566fe1363c7da18f3c0f6f77fb82876a
Christoph Welcz <christoph@welcz.de>
2018-02-07 15:49:42 +0100

    solved verse for 1

--- typescript/beer-song/beer-song.ts
+++ typescript/beer-song/beer-song.ts
@@ -1,10 +1,25 @@
 export default class Beer {
 
     static verse(verseNumber: number) {
-        const currentBottles = verseNumber
-        const remainingBottles = currentBottles - 1
-        return `${currentBottles} bottles of beer on the wall, ${currentBottles} bottles of beer.
-Take one down and pass it around, ${remainingBottles} bottles of beer on the wall.
+        const currentBottles = this.bottlesWord(verseNumber)
+        const remainingBottles = this.bottlesWord(verseNumber - 1)
+        const takeIt = this.takeIt(verseNumber)
+        return `${currentBottles} on the wall, ${currentBottles}.
+Take ${takeIt} down and pass it around, ${remainingBottles} on the wall.
 `
+    }
+
+    private static takeIt(verseNumber: number) {
+        return verseNumber === 1 ? 'it' : `one`
+    }
+
+    private static bottlesWord(numberOfBottles: number) {
+        if (numberOfBottles === 0) {
+            return 'no more bottles of beer'
+        }
+        if (numberOfBottles === 1) {
+            return '1 bottle of beer'
+        }
+        return `${numberOfBottles} bottles of beer`
     }
 }
