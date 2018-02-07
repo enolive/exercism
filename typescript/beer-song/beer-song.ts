4602c864d6ada186db6733065e4ead6d5e66e8ef
Christoph Welcz <christoph@welcz.de>
2018-02-07 19:46:42 +0100

    some minor optimizations

--- typescript/beer-song/beer-song.ts
+++ typescript/beer-song/beer-song.ts
@@ -3,25 +3,26 @@ export default class Beer {
     static sing(start: number, end: number = 0) {
         return this.from(start)
             .to(end)
-            .map((verse) => this.verse(verse))
+            .map((i) => this.verse(i))
             .join('\n')
     }
 
     static verse(verseNumber: number) {
-        return `${this.firstSentence(verseNumber)}
-${this.secondSentence(verseNumber)}.
-`
-    }
-
-    private static secondSentence(verseNumber: number) {
-        return verseNumber === 0
-            ? `Go to the store and buy some more, ${this.bottles(99)} on the wall`
-            : `Take ${this.takeIt(verseNumber)} down and pass it around, ${this.bottles(verseNumber - 1)} on the wall`
+        return [
+            this.firstSentence(verseNumber),
+            this.secondSentence(verseNumber)]
+            .join('\n') + '\n'
     }
 
     private static firstSentence(verseNumber: number) {
         const currentBottles = this.bottles(verseNumber)
         return this.capitalizeFirstLetter(`${currentBottles} on the wall, ${currentBottles}.`)
+    }
+
+    private static secondSentence(verseNumber: number) {
+        return verseNumber === 0
+            ? `Go to the store and buy some more, ${this.bottles(99)} on the wall.`
+            : `Take ${this.takeIt(verseNumber)} down and pass it around, ${this.bottles(verseNumber - 1)} on the wall.`
     }
 
     private static capitalizeFirstLetter(sentence: string) {
@@ -33,13 +34,14 @@ ${this.secondSentence(verseNumber)}.
     }
 
     private static bottles(numberOfBottles: number) {
-        if (numberOfBottles === 0) {
-            return 'no more bottles of beer'
+        switch (numberOfBottles) {
+            case 0:
+                return 'no more bottles of beer'
+            case 1:
+                return '1 bottle of beer'
+            default:
+                return `${numberOfBottles} bottles of beer`
         }
-        if (numberOfBottles === 1) {
-            return '1 bottle of beer'
-        }
-        return `${numberOfBottles} bottles of beer`
     }
 
     private static from(start: number) {
