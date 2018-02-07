723effc2c5d9e45ac70e38772127f8189c987f7b
Christoph Welcz <christoph@welcz.de>
2018-02-07 20:24:05 +0100

    expression style for functions

--- typescript/beer-song/beer-song.ts
+++ typescript/beer-song/beer-song.ts
@@ -4,46 +4,41 @@ const NO_BOTTLES = 0
 
 export default class Beer {
 
-    static sing(start: number = MAX_BOTTLES, end: number = NO_BOTTLES) {
-        return this.from(start)
-            .to(end)
-            .map((i) => this.verse(i))
+    static sing = (start: number = MAX_BOTTLES, end: number = NO_BOTTLES) =>
+        Range.from(start).to(end)
+            .map((i) => Beer.verse(i))
             .join('\n')
+
+    static verse = (bottles: number) =>
+        [Beer.firstSentence(bottles), Beer.secondSentence(bottles)].join('\n') + '\n'
+
+    private static firstSentence = (bottles: number) => {
+        const currentBottles = Beer.bottles(bottles)
+        return Beer.capitalize(`${currentBottles} on the wall, ${currentBottles}.`)
     }
 
-    static verse(verseNumber: number) {
-        return [
-            this.firstSentence(verseNumber),
-            this.secondSentence(verseNumber)]
-            .join('\n') + '\n'
-    }
+    private static secondSentence = (bottles: number) =>
+        bottles === NO_BOTTLES
+            ? `Go to the store and buy some more, ${Beer.bottles(MAX_BOTTLES)} on the wall.`
+            : `Take ${Beer.takeItOrOne(bottles)} down and pass it around, ${Beer.bottles(bottles - 1)} on the wall.`
 
-    private static firstSentence(bottles: number) {
-        const currentBottles = this.bottles(bottles)
-        return this.capitalizeFirstLetter(`${currentBottles} on the wall, ${currentBottles}.`)
-    }
+    private static capitalize = (sentence: string) =>
+        sentence.charAt(0).toUpperCase() + sentence.slice(1)
 
-    private static secondSentence(bottles: number) {
-        return bottles === NO_BOTTLES
-            ? `Go to the store and buy some more, ${this.bottles(MAX_BOTTLES)} on the wall.`
-            : `Take ${this.takeItOrOne(bottles)} down and pass it around, ${this.bottles(bottles - 1)} on the wall.`
-    }
-
-    private static capitalizeFirstLetter(sentence: string) {
-        return sentence.charAt(0).toUpperCase() + sentence.slice(1)
-    }
-
-    private static takeItOrOne(bottles: number) {
+    private static takeItOrOne = (bottles: number) => {
         return bottles === SINGLE_BOTTLE ? 'it' : `one`
     }
 
-    private static bottles(count: number) {
+    private static bottles = (count: number) => {
         const howMany = count === NO_BOTTLES ? 'no more' : count
         const pluralNoun = count === SINGLE_BOTTLE ? '' : 's'
         return `${howMany} bottle${pluralNoun} of beer`
     }
 
-    private static from(start: number) {
+}
+
+class Range {
+    static from(start: number) {
         return {
             to: (end: number) => Array.from({length: start - end + 1}, (_, i) => start - i)
         }
