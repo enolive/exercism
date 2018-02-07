84d568afeafe3e68448018553f7e3ab3b25b687b
Christoph Welcz <christoph@welcz.de>
2018-02-07 20:32:21 +0100

    separation of concerns (Beer, Song lines and utility)

--- typescript/beer-song/beer-song.ts
+++ typescript/beer-song/beer-song.ts
@@ -3,38 +3,42 @@ const SINGLE_BOTTLE = 1
 const NO_BOTTLES = 0
 
 export default class Beer {
-
-    static sing = (start: number = MAX_BOTTLES, end: number = NO_BOTTLES) =>
-        Range.from(start).to(end)
+    static sing(start: number = MAX_BOTTLES, end: number = NO_BOTTLES) {
+        return Range.from(start).to(end)
             .map((i) => Beer.verse(i))
             .join('\n')
-
-    static verse = (bottles: number) =>
-        [Beer.firstSentence(bottles), Beer.secondSentence(bottles)].join('\n') + '\n'
-
-    private static firstSentence = (bottles: number) => {
-        const currentBottles = Beer.bottles(bottles)
-        return Beer.capitalize(`${currentBottles} on the wall, ${currentBottles}.`)
     }
 
-    private static secondSentence = (bottles: number) =>
-        bottles === NO_BOTTLES
-            ? `Go to the store and buy some more, ${Beer.bottles(MAX_BOTTLES)} on the wall.`
-            : `Take ${Beer.takeItOrOne(bottles)} down and pass it around, ${Beer.bottles(bottles - 1)} on the wall.`
+    static verse(bottles: number) {
+        return [SongLines.first(bottles), SongLines.second(bottles)].join('\n') + '\n'
+    }
+}
 
-    private static capitalize = (sentence: string) =>
-        sentence.charAt(0).toUpperCase() + sentence.slice(1)
+class SongLines {
+    static first(bottles: number) {
+        const currentBottles = SongLines.bottles(bottles)
+        return SongLines.capitalize(`${currentBottles} on the wall, ${currentBottles}.`)
+    }
 
-    private static takeItOrOne = (bottles: number) => {
+    static second(bottles: number) {
+        return bottles === NO_BOTTLES
+            ? `Go to the store and buy some more, ${SongLines.bottles(MAX_BOTTLES)} on the wall.`
+            : `Take ${SongLines.takeItOrOne(bottles)} down and pass it around, ${SongLines.bottles(bottles - 1)} on the wall.`
+    }
+
+    private static capitalize(sentence: string) {
+        return sentence.charAt(0).toUpperCase() + sentence.slice(1)
+    }
+
+    private static takeItOrOne(bottles: number) {
         return bottles === SINGLE_BOTTLE ? 'it' : `one`
     }
 
-    private static bottles = (count: number) => {
+    private static bottles(count: number) {
         const howMany = count === NO_BOTTLES ? 'no more' : count
         const pluralNoun = count === SINGLE_BOTTLE ? '' : 's'
         return `${howMany} bottle${pluralNoun} of beer`
     }
-
 }
 
 class Range {
