b0807952e539170401532d5c1c897a37c5c45769
Christoph Welcz <christoph@welcz.de>
2018-02-14 22:07:20 +0100

    reformatting -- pattern matching is here the best solution I think

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,11 +1,13 @@
-module LeapYear (isLeapYear) where
+module LeapYear
+  ( isLeapYear
+  ) where
 
 isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy number divisor = number `mod` divisor == 0
 
 isLeapYear :: Integer -> Bool
 isLeapYear year
-    | year `isDivisibleBy` 400 = True
-    | year `isDivisibleBy` 100 = False
-    | year `isDivisibleBy` 4 = True
-    | otherwise = False
+  | year `isDivisibleBy` 400 = True
+  | year `isDivisibleBy` 100 = False
+  | year `isDivisibleBy` 4 = True
+  | otherwise = False
