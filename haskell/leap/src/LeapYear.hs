4d11573ffd769eabae963946a467bb687da2a105
Christoph Welcz <christoph@welcz.de>
2018-02-14 20:31:51 +0100

    solved with pattern matching

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -1,4 +1,11 @@
 module LeapYear (isLeapYear) where
 
+isDivisibleBy :: Integer -> Integer -> Bool
+isDivisibleBy number divisor = number `mod` divisor == 0
+
 isLeapYear :: Integer -> Bool
-isLeapYear year = error "You need to implement this function."
+isLeapYear year
+    | year `isDivisibleBy` 400 = True
+    | year `isDivisibleBy` 100 = False
+    | year `isDivisibleBy` 4 = True
+    | otherwise = False
