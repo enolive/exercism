8692b73a5b48e5f99ae690a29ef3f7294e5af482
Christoph Welcz <christoph@welcz.de>
2018-02-14 22:23:18 +0100

    removed shortening pattern matching for more readability

--- haskell/leap/src/LeapYear.hs
+++ haskell/leap/src/LeapYear.hs
@@ -5,10 +5,9 @@ module LeapYear
 isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy number divisor = number `mod` divisor == 0
 
-isNotDivisibleBy :: Integer -> Integer -> Bool
-isNotDivisibleBy number divisor = not $ isDivisibleBy number divisor
-
 isLeapYear :: Integer -> Bool
 isLeapYear year
   | year `isDivisibleBy` 400 = True
-  | otherwise = year `isDivisibleBy` 4 && year `isNotDivisibleBy` 100
+  | year `isDivisibleBy` 100 = False
+  | year `isDivisibleBy` 4 = True
+  | otherwise = False
