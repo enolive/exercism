ae8543b8d3c433f20030c631bda4a9688a5107cf
Christoph Welcz <christoph@welcz.de>
2018-02-16 22:57:25 +0100

    moved guard to exported function

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,14 +2,15 @@ module CollatzConjecture
   ( collatz
   ) where
 
-collatzRec :: Integer -> Integer -> Maybe Integer
+collatzRec :: Integer -> Integer -> Integer
 collatzRec n steps
-  | n <= 0 = Nothing
-  | n == 1 = Just steps
+  | n == 1 = steps
   | even n = continueWith $ n `div` 2
   | odd n = continueWith $ n * 3 + 1
   where
     continueWith n = collatzRec n $ steps + 1
 
 collatz :: Integer -> Maybe Integer
-collatz n = collatzRec n 0
+collatz n
+  | n <= 0 = Nothing
+  | otherwise = Just $ collatzRec n 0
