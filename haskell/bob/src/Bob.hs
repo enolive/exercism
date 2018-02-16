c09d8c21c89f30093fec89f57c8065ee59b09024
Christoph Welcz <christoph@welcz.de>
2018-02-16 15:21:16 +0100

    solved simple silence

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -4,11 +4,13 @@ import Data.List (isSuffixOf)
 
 responseFor :: String -> String
 responseFor statement
+    | isSilence = "Fine. Be that way!"
     | isForcefulQuestion = "Calm down, I know what I'm doing!"
     | isQuestion = "Sure."
     | isShouting = "Whoa, chill out!"
     | otherwise = "Whatever."
     where
+    isSilence = statement == ""
     isForcefulQuestion = isQuestion && isShouting
     isShouting = any isLetter statement && statement == map toUpper statement
     isQuestion = "?" `isSuffixOf` statement
