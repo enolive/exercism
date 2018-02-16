e03718222a6a8711406b802d2f85579918f4019a
Christoph Welcz <christoph@welcz.de>
2018-02-16 20:41:25 +0100

    solved some tests

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -2,7 +2,14 @@ module Pangram
   ( isPangram
   ) where
 
+import Data.Char (toLower)
+import Data.HashSet (fromList)
+
 isPangram :: String -> Bool
 isPangram text
-  | null text = False
+  | null characters = False
+  | length characters == length alphabet = True
   | otherwise = True
+  where
+    characters = fromList (map toLower text)
+    alphabet = ['a' .. 'z']
