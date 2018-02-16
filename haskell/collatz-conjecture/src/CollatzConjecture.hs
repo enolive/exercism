b763872a7dcea133f1adb00a0a8f685573715b25
Christoph Welcz <christoph@welcz.de>
2018-02-16 22:47:00 +0100

    some smaller optimizations

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,12 +2,14 @@ module CollatzConjecture
   ( collatz
   ) where
 
+inc :: Integer -> Integer
+inc n = n + 1
+
 collatzRec :: Integer -> Integer -> Maybe Integer
-collatzRec 1 steps = Just steps
 collatzRec n steps
-  | n < 0 = Nothing
-  | even n = collatzRec (n `div` 2) (steps + 1)
-  | odd n = collatzRec (n * 3 + 1) (steps + 1)
+  | n == 1 = Just steps
+  | even n = collatzRec (n `div` 2) $ inc steps
+  | odd n = collatzRec (n * 3 + 1) $ inc steps
 
 collatz :: Integer -> Maybe Integer
 collatz n = collatzRec n 0
