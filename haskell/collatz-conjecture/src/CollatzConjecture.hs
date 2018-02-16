7e94618b15e054be687476b4761f2f3782a57d6e
Christoph Welcz <christoph@welcz.de>
2018-02-17 00:21:13 +0100

    finally understood modify

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,19 +2,18 @@ module CollatzConjecture
   ( collatz
   ) where
 
-import Control.Monad.State.Strict (State, evalState, get, put)
+import Control.Monad.State.Strict (State, evalState, get, modify, put)
 
 collatz :: Integer -> Maybe Integer
 collatz n
   | n <= 0 = Nothing
-  | otherwise = Just $ evalState (countSteps n) 0
+  | otherwise = Just $ evalState (collatzRec n) 0
 
-countSteps :: Integer -> State Integer Integer
-countSteps 1 = get
-countSteps n = do
-  acc <- get
-  put (acc + 1)
-  countSteps $
+collatzRec :: Integer -> State Integer Integer
+collatzRec 1 = get
+collatzRec n = do
+  modify (+ 1)
+  collatzRec $
     if even n
       then n `div` 2
       else n * 3 + 1
