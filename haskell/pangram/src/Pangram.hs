5f7a9fedaef0d4a1ee3d930cad5ca0e1d57b7fe4
Christoph Welcz <christoph@welcz.de>
2018-02-16 20:49:17 +0100

    only characters

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -2,14 +2,14 @@ module Pangram
   ( isPangram
   ) where
 
-import Data.Char (toLower)
+import Data.Char (toLower, isLetter)
 import Data.HashSet (fromList)
 
 isPangram :: String -> Bool
 isPangram text
   | null characters = False
   | length characters == length alphabet = True
-  | otherwise = True
+  | otherwise = False
   where
-    characters = fromList (map toLower text)
+    characters = fromList [toLower c | c <- text, isLetter c]
     alphabet = ['a' .. 'z']
