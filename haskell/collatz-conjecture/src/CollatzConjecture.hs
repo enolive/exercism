8fa700c86be1b7126980ed184d456cae28255cc8
Christoph Welcz <christoph@welcz.de>
2018-02-16 22:51:16 +0100

    removed duplication

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -2,16 +2,14 @@ module CollatzConjecture
   ( collatz
   ) where
 
-inc :: Integer -> Integer
-inc n = n + 1
-
 collatzRec :: Integer -> Integer -> Maybe Integer
 collatzRec n steps
   | n <= 0 = Nothing
   | n == 1 = Just steps
-  | even n = collatzRec (n `div` 2) $ inc steps
-  | odd n = collatzRec (n * 3 + 1) $ inc steps
-
+  | even n = continueWith $ n `div` 2
+  | odd n = continueWith $ n * 3 + 1
+  where
+    continueWith n = collatzRec n $ steps + 1
 
 collatz :: Integer -> Maybe Integer
 collatz n = collatzRec n 0
