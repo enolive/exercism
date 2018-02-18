ad73b0bca9f66adc53ff9460948e259330705bf7
Christoph Welcz <christoph@welcz.de>
2018-02-18 19:24:26 +0100

    solved exercise

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,5 +1,6 @@
 module SumOfMultiples (sumOfMultiples) where
+import Data.List (nub)
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum multiples
-  where multiples = [x | x <- factors, x < limit]
+sumOfMultiples factors limit = sum $ nub $ factors >>= multiplesOf
+  where multiplesOf n = takeWhile (< limit) [n*x | x <- [1..]]
