639b8dfc35eaf419e87317d67dc0b795bc786747
Christoph Welcz <christoph@welcz.de>
2018-02-18 17:55:40 +0100

    removed maybe signature from transformations

--- haskell/rna-transcription/src/DNA.hs
+++ haskell/rna-transcription/src/DNA.hs
@@ -1,7 +1,10 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Maybe String
-toRNA "C" = Just "G"
-toRNA "G" = Just "C"
-toRNA "T" = Just "A"
-toRNA "A" = Just "U"
+toRNA xs = Just $ transform xs
+  where
+  transform :: String -> String
+  transform "C" = "G"
+  transform "G" = "C"
+  transform "T" = "A"
+  transform "A" = "U"
