ee48c53a1450599681a1b87c4c667c69b724f975
Christoph Welcz <christoph@welcz.de>
2018-02-18 18:55:59 +0100

    solved first two exercises

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,5 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = sum multiples
+  where multiples = [x | x <- factors, x < limit]
