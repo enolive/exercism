66757011ba123c44940cc4f019869e7d8cc7264a
Christoph Welcz <christoph@welcz.de>
2018-02-21 13:26:23 +0100

    solved square of sums

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -1,10 +1,14 @@
-module Squares (difference, squareOfSums, sumOfSquares) where
+module Squares
+  ( difference
+  , squareOfSums
+  , sumOfSquares
+  ) where
 
 difference :: Integral a => a -> a
 difference n = error "You need to implement this function."
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = 1
+squareOfSums n = sum [1 .. n] ^ 2
 
 sumOfSquares :: Integral a => a -> a
 sumOfSquares n = error "You need to implement this function."
