6b5754124ef7f55005eb945702576d5f8c16be01
Christoph Welcz <christoph@welcz.de>
2018-02-21 13:50:51 +0100

    made square of sums easier to read using the compose operator

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -8,10 +8,12 @@ difference :: Integral a => a -> a
 difference n = squareOfSums n - sumOfSquares n
 
 squareOfSums :: Integral a => a -> a
-squareOfSums n = squared $ sumUpTo n
-  where squared = flip (^) 2
-        sumUpTo n = n * (n + 1) `div` 2
+squareOfSums = squared . sumUpTo
+  where
+    squared = flip (^) 2
+    sumUpTo n = n * (n + 1) `div` 2
 
 sumOfSquares :: Integral a => a -> a
 sumOfSquares = squarePyramidalNumber
-  where squarePyramidalNumber n = n * (n + 1) * (2 * n + 1) `div` 6
+  where
+    squarePyramidalNumber n = n * (n + 1) * (2 * n + 1) `div` 6
