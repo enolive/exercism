f135cfedbd0d7e42cf66adf87d49bcbc3a1ca1d2
Christoph Welcz <christoph@welcz.de>
2018-02-21 13:47:48 +0100

    solved using the algorithm for square pyramidal number

--- haskell/difference-of-squares/src/Squares.hs
+++ haskell/difference-of-squares/src/Squares.hs
@@ -4,15 +4,14 @@ module Squares
   , sumOfSquares
   ) where
 
-squared :: Integral a => a -> a
-squared = flip (^) 2
-
 difference :: Integral a => a -> a
 difference n = error "You need to implement this function."
 
 squareOfSums :: Integral a => a -> a
 squareOfSums n = squared $ sumUpTo n
-  where sumUpTo n = n * (n + 1) `div` 2
+  where squared = flip (^) 2
+        sumUpTo n = n * (n + 1) `div` 2
 
 sumOfSquares :: Integral a => a -> a
-sumOfSquares n = sum $ fmap squared [1..n]
+sumOfSquares = squarePyramidalNumber
+  where squarePyramidalNumber n = n * (n + 1) * (2 * n + 1) `div` 6
