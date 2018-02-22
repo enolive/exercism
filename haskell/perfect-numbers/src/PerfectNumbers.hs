a2f9e83aa9ef34d41997c594f26e281767f0589b
Christoph Welcz <christoph@welcz.de>
2018-02-22 19:22:31 +0100

    solved perfect right ;-)

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -1,6 +1,17 @@
-module PerfectNumbers (classify, Classification(..)) where
+module PerfectNumbers
+  ( classify
+  , Classification(..)
+  ) where
 
-data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
+data Classification
+  = Deficient
+  | Perfect
+  | Abundant
+  deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify n = Just Perfect
+classify n
+  | sumAliquot n == n = Just Perfect
+  where
+    sumAliquot n = sum $ filter (divides n) $ takeWhile (< n) [1 ..]
+    divides n quot = n `rem` quot == 0
