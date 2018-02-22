b2c9421390746b036bd3c2bf501d3cbb2e030c55
Christoph Welcz <christoph@welcz.de>
2018-02-22 19:30:38 +0100

    solved the whole exercise

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -10,11 +10,11 @@ data Classification
   deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify n =
-  case compare sumAliquot n of
-    EQ -> Just Perfect
-    GT -> Just Abundant
-    LT -> Just Deficient
+classify n
+  | n <= 0 = Nothing
+  | sumAliquot == n = Just Perfect
+  | sumAliquot < n = Just Deficient
+  | sumAliquot > n = Just Abundant
   where
     sumAliquot = sum $ filter dividesInput $ takeWhile (< n) [1 ..]
     dividesInput quot = n `rem` quot == 0
