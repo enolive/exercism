726c393e33e6c2048dab463c4815f908c215f316
Christoph Welcz <christoph@welcz.de>
2018-02-23 20:20:04 +0100

    using a comprehension for even slicker code

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -5,4 +5,4 @@ module Hamming
 distance :: String -> String -> Maybe Int
 distance xs ys
   | length xs /= length ys = Nothing
-  | otherwise = Just $ length $ filter (uncurry (/=)) $ zip xs ys
+  | otherwise = Just $ length [x | x <- zipWith (/=) xs ys, x]
