83c935fcef8f615f2cedf43359ee3be30a4e2938
Christoph Welcz <christoph@welcz.de>
2018-02-23 20:10:07 +0100

    solved positive cases

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,4 +1,9 @@
-module Hamming (distance) where
+module Hamming
+  ( distance
+  ) where
 
 distance :: String -> String -> Maybe Int
-distance xs ys = Just 0
+distance xs ys = Just $ length $ filter areDifferent $ zip xs ys
+  where
+    areDifferent :: (Char, Char) -> Bool
+    areDifferent (x, y) = x /= y
