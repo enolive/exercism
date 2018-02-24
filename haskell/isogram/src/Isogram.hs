c52672f5a1eb7f680a3f5f0c492d935f3bd24d93
Christoph Welcz <christoph@welcz.de>
2018-02-24 19:04:54 +0100

    solved the exercise

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -1,11 +1,13 @@
-module Isogram (isIsogram) where
+module Isogram
+  ( isIsogram
+  ) where
 
-import Data.Char (toLower)
-import Data.Map (Map)
-import qualified Data.Map as Map
+import           Data.Char (isLetter, toLower)
+import           Data.Map  (Map)
+import qualified Data.Map  as Map
 
 isIsogram :: String -> Bool
 isIsogram xs = all (== 1) $ (Map.elems . letterCount) xs
 
 letterCount :: String -> Map Char Int
-letterCount xs = Map.fromListWith (+) [(toLower letter, 1) | letter <- xs]
+letterCount xs = Map.fromListWith (+) [(toLower letter, 1) | letter <- xs, isLetter letter]
