d7a558489b763b891b81e94dfe2e2ac39476f4ce
Christoph Welcz <christoph@welcz.de>
2018-02-24 15:52:38 +0100

    removed SLA violation by introducing a new function for nucleotides

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -2,12 +2,14 @@ module DNA
   ( nucleotideCounts
   ) where
 
-import Data.Map (Map, fromList, fromListWith)
+import Data.Map
+import qualified Data.Map as Map
 
 nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts xs
-  | length empty /= length nucleotideMap = Left "Invalid input"
+  | length empty /= Map.size nucleotideMap = Left "Invalid input"
   | otherwise = Right nucleotideMap
   where
     empty = [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
-    nucleotideMap = fromListWith (+) $ [(c, 1) | c <- xs] ++ empty
+    nucleotides = [(n, 1) | n <- xs]
+    nucleotideMap = Map.fromListWith (+) $ nucleotides ++ empty
