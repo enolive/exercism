de0f4d7809a55302330f1c4c53b13ce7a62ae350
Christoph Welcz <christoph@welcz.de>
2018-02-24 19:44:13 +0100

    made an explaining function for this nub binding

--- haskell/isogram/src/Isogram.hs
+++ haskell/isogram/src/Isogram.hs
@@ -6,7 +6,10 @@ import Data.Char (isLetter, toLower)
 import Data.List (nub)
 
 isIsogram :: String -> Bool
-isIsogram = (nub >>= (==)) . letters
+isIsogram = areAllUnique . letters
+
+areAllUnique :: String -> Bool
+areAllUnique = nub >>= (==)
 
 letters :: String -> String
 letters = map toLower . filter isLetter
