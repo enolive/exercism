4cc066df2d6d679382aaca15836c338d7073b363
Christoph Welcz <christoph@welcz.de>
2018-02-24 15:28:51 +0100

    solved positive cases

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,6 +1,7 @@
 module DNA (nucleotideCounts) where
 
-import Data.Map (Map, fromList)
+import Data.Map (Map, fromList, fromListWith)
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts xs = Right $ fromList [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
+nucleotideCounts xs = Right $ fromListWith (+) $ [(c, 1) | c <- xs] ++ empty
+  where empty = [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
