c6edd6630b9c7b1e1075a2403de7f78c96e5309d
Christoph Welcz <christoph@welcz.de>
2018-02-24 15:33:08 +0100

    solved the exercise

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,7 +1,13 @@
-module DNA (nucleotideCounts) where
+module DNA
+  ( nucleotideCounts
+  ) where
 
 import Data.Map (Map, fromList, fromListWith)
 
 nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts xs = Right $ fromListWith (+) $ [(c, 1) | c <- xs] ++ empty
-  where empty = [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
+nucleotideCounts xs
+  | length empty /= length nucleotideMap = Left "Invalid input"
+  | otherwise = Right nucleotideMap
+  where
+    empty = [('A', 0), ('C', 0), ('G', 0), ('T', 0)]
+    nucleotideMap = fromListWith (+) $ [(c, 1) | c <- xs] ++ empty
