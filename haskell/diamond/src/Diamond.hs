277b9935a482d52c47afa266cd4aeaed51c0c357
Christoph Welcz <christoph@welcz.de>
2018-03-04 19:06:36 +0100

    even less faking

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -6,9 +6,11 @@ diamond :: Char -> Maybe [String]
 diamond 'A' = Just ["A"]
 diamond 'B' = Just [" A ", "B B", " A "]
 diamond 'C' = Just $ mirror [
-  mirror "  A",
-  mirror " B ",
-  mirror "C  "]
+  mirror $ spaces (2 - 0) ++ "A" ++ spaces 0,
+  mirror $ spaces (2 - 1) ++ "B" ++ spaces 1,
+  mirror $ spaces (2 - 2) ++ "C" ++ spaces 2]
+
+spaces n = replicate n ' '
 
 mirror :: [a] -> [a]
 mirror = mirrored >>= flip (++)
