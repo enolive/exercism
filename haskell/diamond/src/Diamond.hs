6563ba31fdb3b4acb151b6ddb790da44e011330d
Christoph Welcz <christoph@welcz.de>
2018-03-04 19:16:07 +0100

    replaced hidden loop with map

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -7,10 +7,8 @@ diamond 'A' = Just ["A"]
 diamond 'B' = Just [" A ", "B B", " A "]
 diamond 'C' = Just $ mirror upperHalf
 
-upperHalf = [
-              mirror $ makeLine 2 (0, 'A'),
-              mirror $ makeLine 2 (1, 'B'),
-              mirror $ makeLine 2 (2, 'C')]
+upperHalf = map (mirror . makeLine 2) $ zip [0..] ['A'..'C']
+
 makeLine length (index, character) = spaces (length - index) ++ [character] ++ spaces index
 
 spaces :: Int -> String
