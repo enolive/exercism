f96f9e143250cade00f3ee49e2e7546348505f65
Christoph Welcz <christoph@welcz.de>
2018-03-04 19:19:42 +0100

    removed faking completely from upper half

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -5,10 +5,13 @@ module Diamond
 diamond :: Char -> Maybe [String]
 diamond 'A' = Just ["A"]
 diamond 'B' = Just [" A ", "B B", " A "]
-diamond 'C' = Just $ mirror upperHalf
+diamond 'C' = Just $ mirror $ upperHalf 'C'
 
-upperHalf = map (mirror . makeLine 2) $ zip [0..] ['A'..'C']
+upperHalf endChar = map (mirror . makeLine (distanceFromA endChar)) $ zip [0..] ['A'..endChar]
 
+distanceFromA c = 2
+
+makeLine :: Int -> (Int, Char) -> String
 makeLine length (index, character) = spaces (length - index) ++ [character] ++ spaces index
 
 spaces :: Int -> String
