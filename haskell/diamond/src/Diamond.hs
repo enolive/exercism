b1111ed38c01a9ab27577e6b615e2e7774c9bb29
Christoph Welcz <christoph@welcz.de>
2018-03-04 19:48:25 +0100

    solved the whole exercise

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -5,21 +5,20 @@ module Diamond
 import Data.Char (ord)
 
 diamond :: Char -> Maybe [String]
-diamond c = (mirror . upperHalf) <$> Just c
+diamond c = (mirror . makeUpperHalf) <$> Just c
 
-upperHalf endChar = zipWith (curry line) [0 ..] ['A' .. endChar]
+makeUpperHalf :: Char -> [String]
+makeUpperHalf endChar = zipWith (curry line) [0 ..] ['A' .. endChar]
   where
-    line = mirror . makeLine (distanceFromA endChar)
-
-distanceFromA c = ord c - ord 'A'
+    line = mirror . makeLine distanceFromA
+    distanceFromA = ord endChar - ord 'A'
 
 makeLine :: Int -> (Int, Char) -> String
-makeLine length (index, character) = spaces (length - index) ++ [character] ++ spaces index
+makeLine length (index, character) =
+  spaces (length - index) ++ [character] ++ spaces index
 
 spaces :: Int -> String
 spaces n = replicate n ' '
 
 mirror :: [a] -> [a]
-mirror = mirrored >>= flip (++)
-  where
-    mirrored = reverse . init
+mirror = (reverse . init) >>= flip (++)
