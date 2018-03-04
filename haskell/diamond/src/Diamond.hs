569c51e6cb849a51439b907f05f5718e8de22ac1
Christoph Welcz <christoph@welcz.de>
2018-03-04 19:55:40 +0100

    curried the makeline function

--- haskell/diamond/src/Diamond.hs
+++ haskell/diamond/src/Diamond.hs
@@ -8,14 +8,13 @@ diamond :: Char -> Maybe [String]
 diamond c = (mirror . makeUpperHalf) <$> Just c
 
 makeUpperHalf :: Char -> [String]
-makeUpperHalf endChar = zipWith (curry line) [0 ..] ['A' .. endChar]
+makeUpperHalf endChar = zipWith (makeLine distanceFromA) [0 ..] ['A' .. endChar]
   where
-    line = mirror . makeLine distanceFromA
     distanceFromA = ord endChar - ord 'A'
 
-makeLine :: Int -> (Int, Char) -> String
-makeLine length (index, character) =
-  spaces (length - index) ++ [character] ++ spaces index
+makeLine :: Int -> Int -> Char -> String
+makeLine length index character =
+  mirror $ spaces (length - index) ++ [character] ++ spaces index
 
 spaces :: Int -> String
 spaces n = replicate n ' '
