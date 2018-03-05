b195aedbdf1e24ffea8d79021fe21b9bb149b70a
Christoph Welcz <christoph@welcz.de>
2018-03-06 00:44:17 +0100

    solved everything

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -4,14 +4,36 @@ import Data.Char (isDigit)
 
 number :: String -> Maybe String
 number xs
-  | length cleaned < 10 = Nothing
-  | length cleaned == 11 && head cleaned /= '1' = Nothing
-  | length cleaned > 11 = Nothing
-  | any (not . isDigit) cleaned = Nothing
-  | head cleaned == '1' = Just $ tail cleaned
+  | inValid cleaned = Nothing
+  | hasCountryCode cleaned = Just $ tail cleaned
   | otherwise = Just cleaned
   where
     cleaned = clean xs
+
+inValid cleaned
+  | length cleaned `notElem` [10, 11] = True
+  | any (not . isDigit) cleaned = True
+  | countryCode cleaned /= '1' = True
+  | areaCode cleaned `elem` "01" = True
+  | exchangeCode cleaned `elem` "01" = True
+  | otherwise = False
+
+countryCode :: String -> Char
+countryCode cleaned
+  | hasCountryCode cleaned = head cleaned
+  | otherwise = '1'
+
+areaCode :: String -> Char
+areaCode cleaned
+  | hasCountryCode cleaned = (!! 1) cleaned
+  | otherwise = head cleaned
+
+exchangeCode :: String -> Char
+exchangeCode cleaned
+  | hasCountryCode cleaned = (!! 4) cleaned
+  | otherwise = (!! 3) cleaned
+
+hasCountryCode cleaned = length cleaned == 11
 
 clean :: String -> String
 clean = filter (not . isPunctuation)
