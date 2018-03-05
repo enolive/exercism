c9c7001a17112e994cc9e826132cc0eda6a71421
Christoph Welcz <christoph@welcz.de>
2018-03-05 22:50:04 +0100

    iterative version of accumulate

--- haskell/accumulate/src/Accumulate.hs
+++ haskell/accumulate/src/Accumulate.hs
@@ -1,5 +1,4 @@
 module Accumulate (accumulate) where
 
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate f [] = []
-accumulate f (x:xs) = f x : accumulate f xs
+accumulate f xs = foldr (\ current acc -> f current : acc) [] xs
