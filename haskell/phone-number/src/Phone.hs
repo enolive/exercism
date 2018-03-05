18bf073beccfe2a3996b694fcb93e7e150e223cf
Christoph Welcz <christoph@welcz.de>
2018-03-06 00:03:03 +0100

    solved more validation cases

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,10 +1,20 @@
 module Phone (number) where
 
+import Data.Char (isDigit)
+
 number :: String -> Maybe String
-number xs = clean <$> Just xs
+number xs
+  | length cleaned < 10 = Nothing
+  | length cleaned == 11 && head cleaned /= '1' = Nothing
+  | length cleaned > 11 = Nothing
+  | any (not . isDigit) cleaned = Nothing
+  | head cleaned == '1' = Just $ tail cleaned
+  | otherwise = Just cleaned
+  where
+    cleaned = clean xs
 
 clean :: String -> String
 clean = filter (not . isPunctuation)
 
 isPunctuation :: Char -> Bool
-isPunctuation c = c `elem` "() -."
+isPunctuation c = c `elem` "+() -."
