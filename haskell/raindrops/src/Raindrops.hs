f4c5121ab72e113490f765aadac1df301789e230
Christoph Welcz <christoph@welcz.de>
2018-03-05 21:07:19 +0100

    extracted rules

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -7,5 +7,12 @@ convert n
  | n `divisibleBy` 7 = "Plong"
  | otherwise = show n
 
+rules :: [(Int, String)]
+rules = [
+  (3, "Pling"),
+  (5, "Plang"),
+  (7, "Plong")
+  ]
+
 divisibleBy :: Int -> Int -> Bool
 divisibleBy n d = n `mod` d == 0
