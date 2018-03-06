c0010d16d73af8a703a894fc25313077c1440478
Christoph Welcz <christoph@welcz.de>
2018-03-06 02:08:32 +0100

    version using regex

--- haskell/phone-number/src/Phone.hs
+++ haskell/phone-number/src/Phone.hs
@@ -1,36 +1,16 @@
 module Phone (number) where
 
-import Data.Char (isDigit)
+import Text.Regex.Posix ((=~)) -- from regex-posix
 
 number :: String -> Maybe String
-number xs = stripPunctuation xs >>= stripCountryCode >>= validateArea >>= validateExchange
+number xs = getMatch $ cleanNumber xs =~ pat
 
-stripPunctuation :: String -> Maybe String
-stripPunctuation xs
-  | length cleaned < 10 || length cleaned > 11 = Nothing
-  | any (not . isDigit) cleaned = Nothing
-  | otherwise = Just cleaned
-  where
-    cleaned = cleanNumber xs
+getMatch :: (String, String, String, [String]) -> Maybe String
+getMatch (_, _, _, []) = Nothing
+getMatch (_, _, _, groups) = Just $ groups !! 1
+
+pat :: String
+pat = "^(1)?(([2-9][0-9]{2})([2-9][0-9]{6}))$"
 
 cleanNumber :: String -> String
 cleanNumber = filter (`notElem` "+() -.")
-
-stripCountryCode :: String -> Maybe String
-stripCountryCode xs
-  | length xs < 11 = Just xs
-  | head xs == '1' = Just $ tail xs
-  | otherwise = Nothing
-
-validateArea :: String -> Maybe String
-validateArea xs
-  | isValidSubCode $ head xs = Just xs
-  | otherwise = Nothing
-
-validateExchange :: String -> Maybe String
-validateExchange xs
-  | isValidSubCode $ xs !! 3 = Just xs
-  | otherwise = Nothing
-
-isValidSubCode :: Char -> Bool
-isValidSubCode c = c `elem` ['2'..'9']
