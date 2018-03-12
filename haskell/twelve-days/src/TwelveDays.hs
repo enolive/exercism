16314dfec32965761e9f244a4deef451e9c1b8f3
Christoph Welcz <christoph@welcz.de>
2018-03-12 18:45:38 +0100

    implemented verse 5

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -6,14 +6,15 @@ recite :: Int -> Int -> [String]
 recite start stop = [verse start]
 
 verse :: Int -> String
-verse n = "On the " ++ day n ++ " day of Christmas my true love gave to me, " ++ presentList n ++ "."
+verse n = "On the " ++ nth n ++ " day of Christmas my true love gave to me, " ++ presentList n ++ "."
 
-day :: Int -> String
-day n = case n of
+nth :: Int -> String
+nth n = case n of
   1 -> "first"
   2 -> "second"
   3 -> "third"
   4 -> "fourth"
+  5 -> "fifth"
 
 presentList :: Int -> String
 presentList = joinWithCommas . reverse . map present . enumFromTo 1
@@ -27,3 +28,4 @@ present 1 = "a Partridge in a Pear Tree"
 present 2 = "two Turtle Doves"
 present 3 = "three French Hens"
 present 4 = "four Calling Birds"
+present 5 = "five Gold Rings"
