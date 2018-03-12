24e21c76c674bd08e530755dcf19c9c3eebc83d5
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:44:14 +0100

    implemented through faking verse 4

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -11,13 +11,16 @@ day n = case n of
   1 -> "first"
   2 -> "second"
   3 -> "third"
+  4 -> "fourth"
 
 presentList :: Int -> String
 presentList 1 = present 1
 presentList 2 = present 2 ++ ", and " ++ present 1
 presentList 3 = present 3 ++ ", " ++ present 2 ++ ", and " ++ present 1
+presentList 4 = present 4 ++ ", " ++ present 3 ++ ", " ++ present 2 ++ ", and " ++ present 1
 
 present :: Int -> String
 present 1 = "a Partridge in a Pear Tree"
 present 2 = "two Turtle Doves"
 present 3 = "three French Hens"
+present 4 = "four Calling Birds"
