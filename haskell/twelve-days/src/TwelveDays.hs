7c2688a831864c6aa9dcee0ae6bf51c98f2e2fde
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:35:07 +0100

    implemented a present

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -4,11 +4,15 @@ recite :: Int -> Int -> [String]
 recite start stop = [verse start]
 
 verse :: Int -> String
-verse 1 = "On the " ++ day 1 ++ " day of Christmas my true love gave to me, a Partridge in a Pear Tree."
+verse 1 = "On the " ++ day 1 ++ " day of Christmas my true love gave to me, " ++ present 1 ++ "."
 verse 2 = "On the " ++ day 2 ++ " day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
 verse 3 = "On the " ++ day 3 ++ " day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
 
+day :: Int -> String
 day n = case n of
   1 -> "first"
   2 -> "second"
   3 -> "third"
+
+present :: Int -> String
+present 1 = "a Partridge in a Pear Tree"
