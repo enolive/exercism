6644ffdaf829c791d46a42658cd4fa5038c063c8
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:33:19 +0100

    extracted day translation

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -4,6 +4,11 @@ recite :: Int -> Int -> [String]
 recite start stop = [verse start]
 
 verse :: Int -> String
-verse 1 = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."
-verse 2 = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
-verse 3 = "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+verse 1 = "On the " ++ day 1 ++ " day of Christmas my true love gave to me, a Partridge in a Pear Tree."
+verse 2 = "On the " ++ day 2 ++ " day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
+verse 3 = "On the " ++ day 3 ++ " day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+
+day n = case n of
+  1 -> "first"
+  2 -> "second"
+  3 -> "third"
