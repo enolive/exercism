24c469a6ef250c27a73b23f8e962f4dbe09e44a1
Christoph Welcz <christoph@welcz.de>
2018-03-12 21:30:12 +0100

    some minor improvements

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -3,44 +3,37 @@ module TwelveDays
   ) where
 
 import Data.List (intercalate)
+import Text.Printf (printf)
 
-dailyPresents :: [(String, String)]
-dailyPresents =
-  [ ("first", "a Partridge in a Pear Tree")
-  , ("second", "two Turtle Doves")
-  , ("third", "three French Hens")
-  , ("fourth", "four Calling Birds")
-  , ("fifth", "five Gold Rings")
-  , ("sixth", "six Geese-a-Laying")
-  , ("seventh", "seven Swans-a-Swimming")
-  , ("eighth", "eight Maids-a-Milking")
-  , ("ninth", "nine Ladies Dancing")
-  , ("tenth", "ten Lords-a-Leaping")
-  , ("eleventh", "eleven Pipers Piping")
-  , ("twelfth", "twelve Drummers Drumming")
-  ]
+day :: Int -> (String, String)
+day n = case n of
+  1 -> ("first", "a Partridge in a Pear Tree")
+  2 -> ("second", "two Turtle Doves")
+  3 -> ("third", "three French Hens")
+  4 -> ("fourth", "four Calling Birds")
+  5 -> ("fifth", "five Gold Rings")
+  6 -> ("sixth", "six Geese-a-Laying")
+  7 -> ("seventh", "seven Swans-a-Swimming")
+  8 -> ("eighth", "eight Maids-a-Milking")
+  9 -> ("ninth", "nine Ladies Dancing")
+  10 -> ("tenth", "ten Lords-a-Leaping")
+  11 -> ("eleventh", "eleven Pipers Piping")
+  12 -> ("twelfth", "twelve Drummers Drumming")
 
 recite :: Int -> Int -> [String]
 recite start stop = verse <$> [start .. stop]
 
 verse :: Int -> String
-verse n = "On the " ++ nth n ++ " day of Christmas my true love gave to me, " ++ presentList n ++ "."
+verse n = printf "On the %s day of Christmas my true love gave to me, %s." nth gifts
+  where
+    nth = (fst . day) n
+    gifts = giftsUpTo n
 
-fixed :: String -> Int -> String
-fixed str _ = str
-
-nth :: Int -> String
-nth = fst . daily
-
-presentList :: Int -> String
-presentList = joinWithCommas . reverse . map present . enumFromTo 1
+giftsUpTo :: Int -> String
+giftsUpTo = joinWithCommas . reverse . map giftOfTheDay . enumFromTo 1
+  where
+    giftOfTheDay = snd . day
 
 joinWithCommas :: [String] -> String
 joinWithCommas [x] = x
 joinWithCommas xs = intercalate ", " (init xs) ++ ", and " ++ last xs
-
-present :: Int -> String
-present = snd . daily
-
-daily :: Int -> (String, String)
-daily n = dailyPresents !! (n - 1)
