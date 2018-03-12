2f45a2ce720efed5b0e41cd186293302b28b0bb1
Christoph Welcz <christoph@welcz.de>
2018-03-12 18:48:22 +0100

    converted present to a case function

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -16,6 +16,14 @@ nth n = case n of
   4 -> "fourth"
   5 -> "fifth"
 
+present :: Int -> String
+present n = case n of
+  1 -> "a Partridge in a Pear Tree"
+  2 -> "two Turtle Doves"
+  3 -> "three French Hens"
+  4 -> "four Calling Birds"
+  5 -> "five Gold Rings"
+
 presentList :: Int -> String
 presentList = joinWithCommas . reverse . map present . enumFromTo 1
 
@@ -23,9 +31,3 @@ joinWithCommas :: [String] -> String
 joinWithCommas [x] = x
 joinWithCommas xs = intercalate ", " (init xs) ++ ", and " ++ last xs
 
-present :: Int -> String
-present 1 = "a Partridge in a Pear Tree"
-present 2 = "two Turtle Doves"
-present 3 = "three French Hens"
-present 4 = "four Calling Birds"
-present 5 = "five Gold Rings"
