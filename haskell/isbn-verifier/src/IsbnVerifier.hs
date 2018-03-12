b13c60bc4c2730faa23d873317efc5ce59357c4f
Christoph Welcz <christoph@welcz.de>
2018-03-12 22:21:45 +0100

    solved the exercise

--- haskell/isbn-verifier/src/IsbnVerifier.hs
+++ haskell/isbn-verifier/src/IsbnVerifier.hs
@@ -1,4 +1,17 @@
 module IsbnVerifier (isbn) where
+import Data.Char (isDigit, digitToInt)
 
 isbn :: String -> Bool
-isbn = error "You need to implement this function."
+isbn input = validLength && validCheckSum
+  where
+    validLength = length cleanInput == 10
+    validCheckSum = sum digitValues `mod` 11 == 0
+    digitValues = zipWith (*) allDigits $ reverse [1 .. 10]
+    allDigits = numberDigits ++ [checkDigit]
+    numberDigits = (map digitToInt . filter isDigit) $ init cleanInput
+    checkDigit
+      | isDigit lastDigit = digitToInt lastDigit
+      | lastDigit == 'X' = 10
+      | otherwise = 0
+    lastDigit = last cleanInput
+    cleanInput = filter (/= '-') input
