fc62b58bd2741349980884b98492df6e7fb93c59
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:31:04 +0100

    solved the first few verses

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -1,4 +1,9 @@
 module TwelveDays (recite) where
 
 recite :: Int -> Int -> [String]
-recite 1 1 = ["On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."]
+recite start stop = [verse start]
+
+verse :: Int -> String
+verse 1 = "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree."
+verse 2 = "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
+verse 3 = "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
