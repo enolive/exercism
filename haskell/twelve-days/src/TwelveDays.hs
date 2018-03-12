7b22b695a418e35f11ffe069010e02012d14dbe9
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:42:30 +0100

    removed further the faking

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -4,15 +4,18 @@ recite :: Int -> Int -> [String]
 recite start stop = [verse start]
 
 verse :: Int -> String
-verse 1 = "On the " ++ day 1 ++ " day of Christmas my true love gave to me, " ++ present 1 ++ "."
-verse 2 = "On the " ++ day 2 ++ " day of Christmas my true love gave to me, " ++ present 2 ++ ", and " ++ present 1 ++ "."
-verse 3 = "On the " ++ day 3 ++ " day of Christmas my true love gave to me, " ++ present 3 ++ ", " ++ present 2 ++ ", and " ++ present 1 ++ "."
+verse n = "On the " ++ day n ++ " day of Christmas my true love gave to me, " ++ presentList n ++ "."
 
 day :: Int -> String
 day n = case n of
   1 -> "first"
   2 -> "second"
   3 -> "third"
+
+presentList :: Int -> String
+presentList 1 = present 1
+presentList 2 = present 2 ++ ", and " ++ present 1
+presentList 3 = present 3 ++ ", " ++ present 2 ++ ", and " ++ present 1
 
 present :: Int -> String
 present 1 = "a Partridge in a Pear Tree"
