4c97ddace57478a55d01ea69060b51820d568392
Christoph Welcz <christoph@welcz.de>
2018-03-12 16:37:36 +0100

    implemented more than one present

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -5,8 +5,8 @@ recite start stop = [verse start]
 
 verse :: Int -> String
 verse 1 = "On the " ++ day 1 ++ " day of Christmas my true love gave to me, " ++ present 1 ++ "."
-verse 2 = "On the " ++ day 2 ++ " day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree."
-verse 3 = "On the " ++ day 3 ++ " day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+verse 2 = "On the " ++ day 2 ++ " day of Christmas my true love gave to me, " ++ present 2 ++ ", and " ++ present 1 ++ "."
+verse 3 = "On the " ++ day 3 ++ " day of Christmas my true love gave to me, " ++ present 3 ++ ", " ++ present 2 ++ ", and " ++ present 1 ++ "."
 
 day :: Int -> String
 day n = case n of
@@ -16,3 +16,5 @@ day n = case n of
 
 present :: Int -> String
 present 1 = "a Partridge in a Pear Tree"
+present 2 = "two Turtle Doves"
+present 3 = "three French Hens"
