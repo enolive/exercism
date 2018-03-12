86738e233e9f9102006da39e38e88cfa7959e115
Christoph Welcz <christoph@welcz.de>
2018-03-12 18:53:07 +0100

    extracted days to a list of tuples

--- haskell/twelve-days/src/TwelveDays.hs
+++ haskell/twelve-days/src/TwelveDays.hs
@@ -8,21 +8,20 @@ recite start stop = [verse start]
 verse :: Int -> String
 verse n = "On the " ++ nth n ++ " day of Christmas my true love gave to me, " ++ presentList n ++ "."
 
+days :: [(String, String)]
+days = [
+  ("first", "a Partridge in a Pear Tree"),
+  ("second", "two Turtle Doves"),
+  ("third", "three French Hens"),
+  ("fourth", "four Calling Birds"),
+  ("fifth", "five Gold Rings")
+  ]
+
 nth :: Int -> String
-nth n = case n of
-  1 -> "first"
-  2 -> "second"
-  3 -> "third"
-  4 -> "fourth"
-  5 -> "fifth"
+nth n = fst (days !! (n - 1))
 
 present :: Int -> String
-present n = case n of
-  1 -> "a Partridge in a Pear Tree"
-  2 -> "two Turtle Doves"
-  3 -> "three French Hens"
-  4 -> "four Calling Birds"
-  5 -> "five Gold Rings"
+present n = snd (days !! (n - 1))
 
 presentList :: Int -> String
 presentList = joinWithCommas . reverse . map present . enumFromTo 1
