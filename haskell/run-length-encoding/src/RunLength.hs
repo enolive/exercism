887a1646134940eb3b5eafe213b8e8b4a21ae81e
Christoph Welcz <christoph@welcz.de>
2018-03-12 23:22:59 +0100

    solved decoding

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,7 +1,16 @@
 module RunLength (decode, encode) where
 
+import Data.Char (isDigit, digitToInt)
+
 decode :: String -> String
-decode encodedText = error "You need to implement this function."
+decode xs = decode' 0 [] xs
+
+decode' :: Int -> String -> String -> String
+decode' _ acc [] = acc
+decode' howMany acc (x:xs)
+  | isDigit x = decode' (10 * howMany + digitToInt x) acc xs
+  | howMany == 0 = decode' 0 (acc ++ [x]) xs
+  | otherwise = decode' 0 (acc ++ replicate howMany x) xs
 
 encode :: String -> String
-encode text = error "You need to implement this function."
+encode "" = ""
