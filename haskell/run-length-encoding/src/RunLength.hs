6e39277fd4ac2aa7d1f9d420c2aeca2cdc799647
Christoph Welcz <christoph@welcz.de>
2018-03-13 01:17:35 +0100

    improved readability by explaining functions

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -1,18 +1,19 @@
 module RunLength (decode, encode) where
 
-import Data.Char (isDigit, digitToInt)
+import Data.Char (isDigit)
 import Data.List (group, groupBy)
 
 decode :: String -> String
-decode [] = []
-decode xs = reverse $ concatMap decodeGroup $ (groupBy includeDigits . reverse) xs
+decode = concatMap decodeGroup . groupByCount
   where
+    groupByCount = reverse . map toGroup . groupBy includeDigits . reverse
+    decodeGroup (char, count) = replicate count char
+    toGroup [char] = (char, 1)
+    toGroup (char:reversedCount) = (char, (read . reverse) reversedCount)
     includeDigits _ = isDigit
-    decodeGroup [char] = [char]
-    decodeGroup (char:reversedCount) = replicate (read $ reverse reversedCount) char
 
 encode :: String -> String
-encode xs = concatMap encodeGroup $ group xs
+encode = concatMap encodeGroup . group
   where
     encodeGroup [char] = [char]
     encodeGroup g = show (length g) ++ [head g]
