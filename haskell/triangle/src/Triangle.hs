02849f4e911fc5c184e945d974681c39012b444d
Christoph Welcz <christoph@welcz.de>
2018-03-14 22:52:11 +0100

    solved everything

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -1,10 +1,21 @@
 module Triangle (TriangleType(..), triangleType) where
 
+import Data.Set ()
+import qualified Data.Set as Set
+
 data TriangleType = Equilateral
                   | Isosceles
                   | Scalene
                   | Illegal
                   deriving (Eq, Show)
 
-triangleType :: Num a => a -> a -> a -> TriangleType
-triangleType a b c = error "You need to implement this function."
+triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
+triangleType a b c
+  | (== 0) `all` sides = Illegal
+  | a + b < c || b + c < a || a + c < b = Illegal
+  | length unique == 1 = Equilateral
+  | length unique == 2 = Isosceles
+  | length unique == 3 = Scalene
+  where
+    unique = (Set.toList . Set.fromList) sides
+    sides = [a, b, c]
