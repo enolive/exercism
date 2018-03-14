8796853f556486b974ab3a72ca8770871a027e5f
Christoph Welcz <christoph@welcz.de>
2018-03-14 20:53:11 +0100

    extracted beer to a separate function as it is complex enough for that

--- haskell/beer-song/src/Beer.hs
+++ haskell/beer-song/src/Beer.hs
@@ -2,27 +2,32 @@ module Beer
   ( song
   ) where
 
+import Data.Char (toUpper)
+import Data.List (intercalate)
 import Text.Printf (printf)
 
 verse :: Int -> String
-verse remaining = unlines [firstLine, secondLine, ""]
+verse remaining = unlines [firstLine, secondLine]
   where
-    firstLine = beersOnTheWall remaining ++ ", " ++ beers remaining ++ "."
+    firstLine = upperCaseFirst $ beersOnTheWall remaining ++ ", " ++ beers remaining ++ "."
     secondLine
       | remaining == 0 = "Go to the store and buy some more, " ++ beersOnTheWall (99 :: Int) ++ "."
       | otherwise = "Take " ++ oneOrIt ++ " down and pass it around, " ++ beersOnTheWall (remaining - 1) ++ "."
-    beersOnTheWall n = beers n ++ " on the wall"
-    beers n = howMany n ++ " of beer"
+    upperCaseFirst s = (:) ((toUpper . head) s) (tail s)
     oneOrIt
       | remaining == 1 = "it"
       | otherwise = "one"
+
+beersOnTheWall :: Int -> String
+beersOnTheWall n = beers n ++ " on the wall"
+
+beers :: Int -> String
+beers n = howMany n ++ " of beer"
+  where
     howMany n
       | n == 0 = "no more bottles"
       | n == 1 = "1 bottle"
       | otherwise = printf "%d bottles" n
 
 song :: String
-song =
-  concatMap verse [99,98 .. 1] ++
-       "No more bottles of beer on the wall, no more bottles of beer.\n\
-       \Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+song = (intercalate "\n" . map verse) [99,98 .. 0]
