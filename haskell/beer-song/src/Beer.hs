ffe4a913037ad0d66f17a712b2a72ee727de727e
Christoph Welcz <christoph@welcz.de>
2018-03-14 17:39:06 +0100

    made it simpler by removing the verse arithmetic

--- haskell/beer-song/src/Beer.hs
+++ haskell/beer-song/src/Beer.hs
@@ -7,16 +7,15 @@ import Text.Printf (printf)
 verse :: Int -> String
 verse n = unlines [firstLine, secondLine, ""]
   where
-    firstLine = beersOnTheWall currentCount ++ ", " ++ beers currentCount ++ "."
-    secondLine = "Take one down and pass it around, " ++ beersOnTheWall nextCount ++ "."
+    firstLine = beersOnTheWall n ++ ", " ++ beers n ++ "."
+    secondLine = "Take one down and pass it around, " ++ beersOnTheWall (n - 1) ++ "."
     beers = printf "%d bottles of beer"
     beersOnTheWall n = beers n ++ " on the wall"
-    currentCount = 100 - n
-    nextCount = currentCount - 1
 
 song :: String
-song =  concatMap verse [1..97] ++
-       "2 bottles of beer on the wall, 2 bottles of beer.\n\
+song =
+  concatMap verse [99,98 .. 3] ++
+  "2 bottles of beer on the wall, 2 bottles of beer.\n\
        \Take one down and pass it around, 1 bottle of beer on the wall.\n\
        \\n\
        \1 bottle of beer on the wall, 1 bottle of beer.\n\
