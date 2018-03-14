8102ec19f4a6a3157f6ae2da8770aa7db84b56d7
Christoph Welcz <christoph@welcz.de>
2018-03-14 23:03:29 +0100

    removed some helper functions

--- haskell/triangle/src/Triangle.hs
+++ haskell/triangle/src/Triangle.hs
@@ -1,22 +1,27 @@
-module Triangle (TriangleType(..), triangleType) where
+module Triangle
+  ( TriangleType(..)
+  , triangleType
+  ) where
 
 import Data.Set ()
 import qualified Data.Set as Set
 
-data TriangleType = Equilateral
-                  | Isosceles
-                  | Scalene
-                  | Illegal
-                  deriving (Eq, Show)
+data TriangleType
+  = Equilateral
+  | Isosceles
+  | Scalene
+  | Illegal
+  deriving (Eq, Show)
 
 triangleType :: (Num a, Ord a) => a -> a -> a -> TriangleType
 triangleType a b c
   | not greaterThanZero || not hasTriangleInequality = Illegal
-  | length unique == 1 = Equilateral
-  | length unique == 2 = Isosceles
-  | length unique == 3 = Scalene
+  | otherwise =
+    case (Set.size . Set.fromList) sides of
+      1 -> Equilateral
+      2 -> Isosceles
+      3 -> Scalene
   where
     hasTriangleInequality = a + b > c && b + c > a && a + c > b
     greaterThanZero = all (> 0) sides
-    unique = (Set.toList . Set.fromList) sides
     sides = [a, b, c]
