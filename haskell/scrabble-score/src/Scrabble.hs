ddb673b1b5ed78e4875705962cce081ecff86301
Christoph Welcz <christoph@welcz.de>
2018-03-18 17:42:10 +0100

    solved lower case

--- haskell/scrabble-score/src/Scrabble.hs
+++ haskell/scrabble-score/src/Scrabble.hs
@@ -1,9 +1,11 @@
 module Scrabble (scoreLetter, scoreWord) where
 
+import Data.Char (toLower)
+
 scoreLetter :: Char -> Integer
 scoreLetter 'a' = 1
 scoreLetter 'Z' = 10
 scoreLetter _ = 0
 
 scoreWord :: String -> Integer
-scoreWord = sum . map scoreLetter
+scoreWord = sum . map (scoreLetter . toLower)
