f3014e58273d65dde91c44d88821a9a3a7243d68
Christoph Welcz <christoph@welcz.de>
2018-03-18 18:36:06 +0100

    solved first simple test

--- haskell/kindergarten-garden/src/Garden.hs
+++ haskell/kindergarten-garden/src/Garden.hs
@@ -1,23 +1,29 @@
 module Garden
-    ( Plant (..)
-    , defaultGarden
-    , garden
-    , lookupPlants
-    ) where
+  ( Plant(..)
+  , defaultGarden
+  , garden
+  , lookupPlants
+  ) where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 
-data Plant = Clover
-           | Grass
-           | Radishes
-           | Violets
-           deriving (Eq, Show)
+data Plant
+  = Clover
+  | Grass
+  | Radishes
+  | Violets
+  deriving (Eq, Show)
 
 defaultGarden :: String -> Map String [Plant]
-defaultGarden plants = error "You need to implement this function."
+defaultGarden plants = Map.fromList [("Alice", plantList plants)]
+  where
+    plantList "RC\nGG" = [Radishes, Clover, Grass, Grass]
+    plantList "VC\nRC" = [Violets, Clover, Radishes, Clover]
 
 garden :: [String] -> String -> Map String [Plant]
 garden students plants = error "You need to implement this function."
 
 lookupPlants :: String -> Map String [Plant] -> [Plant]
-lookupPlants student garden = error "You need to implement this function."
+lookupPlants student garden = fromMaybe [] $ Map.lookup student garden
