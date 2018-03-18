074af31c163ce30c5284e1482189380d1e1195f8
Christoph Welcz <christoph@welcz.de>
2018-03-18 17:49:37 +0100

    changed case to guards

--- haskell/scrabble-score/src/Scrabble.hs
+++ haskell/scrabble-score/src/Scrabble.hs
@@ -6,13 +6,13 @@ module Scrabble
 import Data.Char (toLower)
 
 scoreLetter :: Char -> Integer
-scoreLetter l =
-  case toLower l of
-    'a' -> 1
-    't' -> 1
-    'f' -> 4
-    'z' -> 10
-    _ -> 0
+scoreLetter l
+  | lower `elem` "at" = 1
+  | lower `elem` "f" = 4
+  | lower `elem` "z" = 10
+  | otherwise = 0
+  where
+    lower = toLower l
 
 scoreWord :: String -> Integer
 scoreWord = sum . map scoreLetter
