84c04426f626a24f3e6a8967c4ccb70cdd4f9d1d
Christoph Welcz <christoph@welcz.de>
2018-03-22 09:48:56 +0100

    solved tests with only one single action

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -3,3 +3,5 @@ module SecretHandshake (handshake) where
 handshake :: Int -> [String]
 handshake 1 = ["wink"]
 handshake 2 = ["double blink"]
+handshake 4 = ["close your eyes"]
+handshake 8 = ["jump"]
