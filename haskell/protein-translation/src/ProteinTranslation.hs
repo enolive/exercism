1cc3e0660cbfea736c85ddf9ace8924e1eebc916
Christoph Welcz <christoph@welcz.de>
2018-03-23 17:51:43 +0100

    implemented all necessary proteins

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -9,6 +9,17 @@ protein :: String -> String
 protein "AUG" = "Methionine"
 protein "UUU" = "Phenylalanine"
 protein "UUC" = "Phenylalanine"
+protein "UUA" = "Leucine"
+protein "UUG" = "Leucine"
+protein "UCU" = "Serine"
+protein "UCC" = "Serine"
+protein "UCA" = "Serine"
+protein "UCG" = "Serine"
+protein "UAU" = "Tyrosine"
+protein "UAC" = "Tyrosine"
+protein "UGU" = "Cysteine"
+protein "UGC" = "Cysteine"
+protein "UGG" = "Tryptophan"
 
 chunks :: Int -> [a] -> [[a]]
 chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
