d006949d7b7a833728a4384c6eb87e67167db0be
Christoph Welcz <christoph@welcz.de>
2018-03-23 17:44:39 +0100

    extracted protein translation prepared chunk the string into groups of 3 elements each

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -1,7 +1,14 @@
 module ProteinTranslation(proteins) where
 
+import Data.List (unfoldr)
+
 proteins :: String -> Maybe [String]
 proteins p = Just [protein p]
 
+protein :: String -> String
 protein "AUG" = "Methionine"
 protein "UUU" = "Phenylalanine"
+protein "UUC" = "Phenylalanine"
+
+chunks :: Int -> [a] -> [[a]]
+chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
