50ff8cdb83ed0847ba9e53057f8397f4d9e12c25
Christoph Welcz <christoph@welcz.de>
2018-03-23 19:39:23 +0100

    removed own implementation of chunks

--- haskell/protein-translation/src/ProteinTranslation.hs
+++ haskell/protein-translation/src/ProteinTranslation.hs
@@ -3,9 +3,10 @@ module ProteinTranslation
   ) where
 
 import Data.List (unfoldr)
+import Data.List.Split (chunksOf)
 
 proteins :: String -> Maybe [String]
-proteins = Just . takeWhile (not . null) . map protein . chunks 3
+proteins = Just . takeWhile (not . null) . map protein . chunksOf 3
 
 protein :: String -> String
 protein chunk
@@ -17,6 +18,3 @@ protein chunk
   | chunk `elem` ["UAU", "UAC"] = "Tyrosine"
   | chunk `elem` ["UGU", "UGC"] = "Cysteine"
   | chunk `elem` ["UAA", "UAG", "UGA"] = []
-
-chunks :: Int -> [a] -> [[a]]
-chunks n = takeWhile (not . null) . unfoldr (Just . splitAt n)
