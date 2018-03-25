cb3172489f780c505e23e5ba12ef62a7d34a66f6
Christoph Welcz <christoph@welcz.de>
2018-03-25 19:10:51 +0200

    solved the exercise

--- haskell/anagram/src/Anagram.hs
+++ haskell/anagram/src/Anagram.hs
@@ -1,4 +1,13 @@
 module Anagram (anagramsFor) where
 
+import Data.List (sort)
+import Data.Char (toLower)
+
 anagramsFor :: String -> [String] -> [String]
-anagramsFor xs xss = error "You need to implement this function."
+anagramsFor word = filter (isAnagramOf word)
+
+isAnagramOf :: String -> String -> Bool
+isAnagramOf word candidate = lowerWord /= lowerCandidate && sort lowerWord == sort lowerCandidate
+  where
+    lowerWord = map toLower word
+    lowerCandidate = map toLower candidate
