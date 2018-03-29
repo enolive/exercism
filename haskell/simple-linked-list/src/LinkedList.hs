4b634660a879b19164938aa25101e6bc6a695d62
Christoph Welcz <christoph@welcz.de>
2018-03-29 22:15:13 +0200

    solved the whole exercise

--- haskell/simple-linked-list/src/LinkedList.hs
+++ haskell/simple-linked-list/src/LinkedList.hs
@@ -1,43 +1,47 @@
 module LinkedList
-    ( LinkedList
-    , datum
-    , fromList
-    , isNil
-    , new
-    , next
-    , nil
-    , reverseLinkedList
-    , toList
-    ) where
+  ( LinkedList
+  , datum
+  , fromList
+  , isNil
+  , new
+  , next
+  , nil
+  , reverseLinkedList
+  , toList
+  ) where
 
-import Data.Maybe
+import Data.List (unfoldr)
+import Data.Maybe (fromJust, fromMaybe, isNothing)
 
-data LinkedList a = LinkedList {
-  rest :: Maybe (LinkedList a),
-  current :: Maybe a
-} deriving (Eq, Show)
+data LinkedList a = LinkedList
+  { rest :: Maybe (LinkedList a)
+  , current :: Maybe a
+  } deriving (Eq, Show)
 
 datum :: LinkedList a -> a
 datum = fromJust . current
 
 fromList :: [a] -> LinkedList a
-fromList xs = error "You need to implement this function."
+fromList = foldr new nil
 
 isNil :: LinkedList a -> Bool
 isNil = isNothing . rest
 
 new :: a -> LinkedList a -> LinkedList a
-new x linkedList = LinkedList { current = Just x, rest = Just linkedList }
+new x linkedList = LinkedList {current = Just x, rest = Just linkedList}
 
 next :: LinkedList a -> LinkedList a
 next = fromMaybe nil . rest
 
 nil :: LinkedList a
-nil = LinkedList { current = Nothing, rest = Nothing }
+nil = LinkedList {current = Nothing, rest = Nothing}
 
 reverseLinkedList :: LinkedList a -> LinkedList a
-reverseLinkedList linkedList = error "You need to implement this function."
+reverseLinkedList = fromList . reverse . toList
 
 toList :: LinkedList a -> [a]
-toList LinkedList { rest = Nothing } = []
-toList linkedList = [datum linkedList] ++ (toList . next) linkedList
+toList = unfoldr getElem
+  where
+    getElem linkedList
+      | isNil linkedList = Nothing
+      | otherwise = Just (datum linkedList, next linkedList)
