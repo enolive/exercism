7d8a406380a5f3f9510d73bc76c3911eaa9c7e51
Christoph Welcz <christoph@welcz.de>
2018-04-08 19:39:12 +0200

    solved the exercise

--- haskell/meetup/src/Meetup.hs
+++ haskell/meetup/src/Meetup.hs
@@ -1,44 +1,55 @@
-module Meetup (Weekday(..), Schedule(..), meetupDay) where
+module Meetup
+  ( Weekday(..)
+  , Schedule(..)
+  , meetupDay
+  ) where
 
 import Data.Time.Calendar (Day, fromGregorian)
 import Data.Time.Calendar.WeekDate (toWeekDate)
 
-data Weekday = Monday
-             | Tuesday
-             | Wednesday
-             | Thursday
-             | Friday
-             | Saturday
-             | Sunday
-             deriving (Show, Eq)
+data Weekday
+  = Monday
+  | Tuesday
+  | Wednesday
+  | Thursday
+  | Friday
+  | Saturday
+  | Sunday
+  deriving (Show, Eq)
 
-data Schedule = First
-              | Second
-              | Third
-              | Fourth
-              | Last
-              | Teenth
+data Schedule
+  = First
+  | Second
+  | Third
+  | Fourth
+  | Last
+  | Teenth
 
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
 meetupDay schedule weekday year month = fromGregorian year month $ day schedule
   where
-    day Teenth = getDay 0 [13 .. 19]
-    day First = getDay 0 [1 .. 31]
-    day Second = getDay 1 [1 .. 31]
-    day Third = getDay 2 [1 .. 31]
-    getDay dropWeeks = (!! dropWeeks) . filter (matches weekday year month)
-
-getWeekDay :: Int -> Weekday
-getWeekDay 1 = Monday
-getWeekDay 2 = Tuesday
-getWeekDay 3 = Wednesday
-getWeekDay 4 = Thursday
-getWeekDay 5 = Friday
-getWeekDay 6 = Saturday
-getWeekDay 7 = Sunday
-getWeekDay _ = error "invalid day"
+    day Teenth = findDay head [13 .. 19]
+    day First = findDay head [1 .. 31]
+    day Second = findDay (!! 1) [1 .. 31]
+    day Third = findDay (!! 2) [1 .. 31]
+    day Fourth = findDay (!! 3) [1 .. 31]
+    day Last = findDay last [1 .. 31]
+    findDay f = f . daysForWeekday
+    daysForWeekday = filter (matches weekday year month)
 
 matches :: Weekday -> Integer -> Int -> Int -> Bool
-matches expected year month dayOfMonth = getWeekDay weekDay == expected
+matches expectedWeekDay year month dayOfMonth = getWeekDay weekDay == expectedWeekDay
   where
     (_, _, weekDay) = toWeekDate $ fromGregorian year month dayOfMonth
+
+getWeekDay :: Int -> Weekday
+getWeekDay dayNumber =
+  case dayNumber of
+    1 -> Monday
+    2 -> Tuesday
+    3 -> Wednesday
+    4 -> Thursday
+    5 -> Friday
+    6 -> Saturday
+    7 -> Sunday
+    _ -> error "invalid day"
