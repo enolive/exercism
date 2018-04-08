f0b7236b53e537efccde14e38f28aa6692d45bb2
Christoph Welcz <christoph@welcz.de>
2018-04-08 20:55:29 +0200

    generalized first cases

--- haskell/roman-numerals/src/Roman.hs
+++ haskell/roman-numerals/src/Roman.hs
@@ -1,6 +1,5 @@
 module Roman (numerals) where
 
 numerals :: Integer -> Maybe String
-numerals 1 = Just "I"
-numerals 2 = Just "II"
-numerals 3 = Just "III"
+numerals n
+ | n < 4 = Just $ replicate (fromInteger n) 'I'
