66bf9caba081af9a18c3af9e9963cc85e54f5319
Christoph Welcz <christoph@welcz.de>
2018-04-08 21:41:59 +0200

    solved the exercise

--- haskell/roman-numerals/src/Roman.hs
+++ haskell/roman-numerals/src/Roman.hs
@@ -1,5 +1,28 @@
 module Roman (numerals) where
 
 numerals :: Integer -> Maybe String
-numerals n
- | n < 4 = Just $ replicate (fromInteger n) 'I'
+numerals n = (Just . snd . foldr translate (n, [])) translationMap
+
+translate :: (Int, String) -> (Integer, String) -> (Integer, String)
+translate (arabic, roman) (remaining, result) = (newRemaining, newResult)
+  where
+    newRemaining = remaining `mod` toInteger arabic
+    newResult = result ++ (concat . replicate numberOfDigits) roman
+    numberOfDigits = fromInteger remaining `div` arabic
+
+translationMap :: [(Int, String)]
+translationMap = [
+  (1, "I"),
+  (4, "IV"),
+  (5, "V"),
+  (9, "IX"),
+  (10, "X"),
+  (40, "XL"),
+  (50, "L"),
+  (90, "XC"),
+  (100, "C"),
+  (400, "CD"),
+  (500, "D"),
+  (900, "CM"),
+  (1000, "M")
+  ]
