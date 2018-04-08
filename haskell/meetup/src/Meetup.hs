a02ab0e98dcc8557adacffe6680ef20924b231b4
Christoph Welcz <christoph@welcz.de>
2018-04-08 18:22:55 +0200

    solved first test

---
+++ haskell/meetup/src/Meetup.hs
@@ -0,0 +1,21 @@
+module Meetup (Weekday(..), Schedule(..), meetupDay) where
+
+import Data.Time.Calendar (Day, fromGregorian)
+
+data Weekday = Monday
+             | Tuesday
+             | Wednesday
+             | Thursday
+             | Friday
+             | Saturday
+             | Sunday
+
+data Schedule = First
+              | Second
+              | Third
+              | Fourth
+              | Last
+              | Teenth
+
+meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
+meetupDay schedule weekday year month = fromGregorian year month 13
