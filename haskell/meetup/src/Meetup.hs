4a9fa13e8043f44b940c58d8633972001127d285
Christoph Welcz <christoph@welcz.de>
2018-04-08 19:21:59 +0200

    more generalizing

--- haskell/meetup/src/Meetup.hs
+++ haskell/meetup/src/Meetup.hs
@@ -20,13 +20,12 @@ data Schedule = First
               | Teenth
 
 meetupDay :: Schedule -> Weekday -> Integer -> Int -> Day
-meetupDay First weekday year month = fromGregorian year month day
-  where
-    day = head $ filter (matches weekday year month) [1..31]
 meetupDay schedule weekday year month = fromGregorian year month $ day schedule
   where
     day Teenth = head $ filter (matches weekday year month) [13..19]
+    day First = head $ filter (matches weekday year month) [1..31]
 
+getWeekDay :: Int -> Weekday
 getWeekDay 1 = Monday
 getWeekDay 2 = Tuesday
 getWeekDay 3 = Wednesday
@@ -34,6 +33,7 @@ getWeekDay 4 = Thursday
 getWeekDay 5 = Friday
 getWeekDay 6 = Saturday
 getWeekDay 7 = Sunday
+getWeekDay _ = error "invalid day"
 
 matches :: Weekday -> Integer -> Int -> Int -> Bool
 matches expected year month dayOfMonth = getWeekDay weekDay == expected
