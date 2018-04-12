a9d7a9952b6bdcf25291e0a6277a983d8720a62a
Christoph Welcz <christoph@welcz.de>
2018-04-12 19:46:54 +0200

    solved by using a sieve

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,8 +1,9 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors 1 = []
-primeFactors 2 = [2]
-primeFactors 3 = [3]
-primeFactors 9 = [3, 3]
-primeFactors 8 = [2, 2, 2]
+primeFactors n = primeFactors' n [2,3]
+
+primeFactors' n [] = []
+primeFactors' n (current:sieve)
+  | n `mod` current == 0 = current : primeFactors' (n `div` current) (current:sieve)
+  | otherwise = primeFactors' n sieve
