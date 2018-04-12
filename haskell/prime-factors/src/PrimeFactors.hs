8d458fd4c332d46dd9cc58ed2e567624cacd7ef6
Christoph Welcz <christoph@welcz.de>
2018-04-12 21:49:50 +0200

    solved with unfold

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,16 +1,16 @@
 module PrimeFactors (primeFactors) where
+import Data.List (unfoldr)
+
+type CurrentAndLimit = (Integer, Integer)
 
 primeFactors :: Integer -> [Integer]
-primeFactors = factorize []
+primeFactors n = unfoldr getNextFactor (2, n)
 
-factorize :: [Integer] -> Integer -> [Integer]
-factorize found 1 = reverse found
-factorize found n = factorize (next:found) (n `div` next)
+getNextFactor :: CurrentAndLimit -> Maybe (Integer, CurrentAndLimit)
+getNextFactor (_, 1) = Nothing
+getNextFactor (current, n) = Just (next, (next, n `div` next))
   where
-    next = head $ filter (n `isDivisibleBy`) [current..n]
-    current = found `headOr` 2
-    headOr [] value = value
-    headOr (x:_) _ = x
+    next = (head . filter (n `isDivisibleBy`)) [current..]
 
 isDivisibleBy :: Integer -> Integer -> Bool
 isDivisibleBy n d = n `mod` d == 0
