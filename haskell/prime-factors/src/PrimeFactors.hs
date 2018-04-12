f046e3d5cbd3c9e5fd7b5f2298a297f67c4c8adf
Christoph Welcz <christoph@welcz.de>
2018-04-12 19:33:16 +0200

    solved the first few cases

--- haskell/prime-factors/src/PrimeFactors.hs
+++ haskell/prime-factors/src/PrimeFactors.hs
@@ -1,4 +1,9 @@
 module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
-primeFactors n = []
+primeFactors n = [x | x <- [2..n], isPrime x && n `mod` x == 0]
+
+isPrime :: Integer -> Bool
+isPrime x = all (not . divides x) [2..x-1]
+  where
+    divides x n = x `mod` n == 0
