3b9293c699865afa74a6cb46aa45f0d2f4240e03
Christoph Welcz <christoph@welcz.de>
2018-06-07 18:37:16 +0200

    removed obsolete code

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -9,10 +9,6 @@ class ArmstrongNumbers {
             sum = (int) (sum + Math.pow(currentDigit, numberOfDigits));
             remaining = remaining / 10;
         }
-//	    int firstDigit = numberToCheck % 10;
-//	    int secondDigit = numberToCheck / 10;
-//	    int thirdDigit = numberToCheck / 100;
-//	    int sum = (int) (Math.pow(firstDigit, 1) + Math.pow(secondDigit, 2) + Math.pow(thirdDigit, 3));
         return sum == numberToCheck;
     }
 
