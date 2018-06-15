4054d148dc715bcce8d2e9d9babdd89583325af3
Christoph Welcz <christoph@welcz.de>
2018-06-15 17:43:39 +0200

    introduced alphabet

--- java/pangram/src/main/java/PangramChecker.java
+++ java/pangram/src/main/java/PangramChecker.java
@@ -3,12 +3,14 @@ import io.vavr.collection.CharSeq;
 import java.util.function.Function;
 
 class PangramChecker {
+    private final CharSeq alphabet = CharSeq.rangeClosed('a', 'z');
+
     boolean isPangram(String input) {
         return CharSeq
                 .of(input)
                 .filter(Character::isLetter)
                 .map(Character::toLowerCase)
                 .groupBy(Function.identity())
-                .size() == 26;
+                .length() == alphabet.length();
     }
 }
