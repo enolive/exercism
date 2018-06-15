46e14da38d8b2e3248e9dfa413d855dedcce345b
Christoph Welcz <christoph@welcz.de>
2018-06-15 17:27:31 +0200

    using pattern matching

--- java/rna-transcription/src/main/java/RnaTranscription.java
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -1,24 +1,21 @@
 import io.vavr.collection.CharSeq;
 
-import java.text.MessageFormat;
+import static io.vavr.API.*;
 
 class RnaTranscription {
     String transcribe(String dnaStrand) {
-        return CharSeq.of(dnaStrand).map(this::transcribeSymbol).mkString();
+        return CharSeq
+                .of(dnaStrand)
+                .map(this::transcribeSymbol)
+                .mkString();
     }
 
     private char transcribeSymbol(char symbol) {
-        switch (symbol) {
-            case 'G':
-                return 'C';
-            case 'C':
-                return 'G';
-            case 'T':
-                return 'A';
-            case 'A':
-                return 'U';
-            default:
-                throw new IllegalArgumentException(MessageFormat.format("Unknown symbol {0}.", symbol));
-        }
+        return Match(symbol).of(
+                Case($('G'), 'C'),
+                Case($('C'), 'G'),
+                Case($('T'), 'A'),
+                Case($('A'), 'U')
+        );
     }
 }
