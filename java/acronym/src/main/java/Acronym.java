7ae83210ef962d7a0582dd463b34506ca3fa27e1
Christoph Welcz <christoph@welcz.de>
2018-06-16 20:52:39 +0200

    solved the first tests

--- java/acronym/src/main/java/Acronym.java
+++ java/acronym/src/main/java/Acronym.java
@@ -1,10 +1,20 @@
+import io.vavr.collection.CharSeq;
+
 class Acronym {
 
+    private final CharSeq phrase;
+
     Acronym(String phrase) {
+        this.phrase = CharSeq.of(phrase);
     }
 
     String get() {
-        return "PNG";
+        var acronym = phrase
+                .zip(phrase.prepend(' '))
+                .filter(l -> l._2 == ' ')
+                .map(l -> Character.toUpperCase(l._1))
+                .mkString();
+        return acronym;
     }
 
 }
