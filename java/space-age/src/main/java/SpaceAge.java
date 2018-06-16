de89089a65df8e081bd27992d9b9cb46bed0fd7f
Christoph Welcz <christoph@welcz.de>
2018-06-16 18:59:01 +0200

    solved first test

---
+++ java/space-age/src/main/java/SpaceAge.java
@@ -0,0 +1,45 @@
+class SpaceAge {
+
+    private final double seconds;
+
+    SpaceAge(double seconds) {
+        this.seconds = seconds;
+    }
+
+    double getSeconds() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onEarth() {
+        return seconds / (365.25 * 24 * 60 * 60);
+    }
+
+    double onMercury() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onVenus() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onMars() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onJupiter() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onSaturn() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onUranus() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    double onNeptune() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
