715bb43117964682927e621ede335ef8a2f0ab63
Christoph Welcz <christoph@welcz.de>
2018-06-18 20:33:03 +0200

    solved with pattern matching!

--- java/scrabble-score/src/main/java/Scrabble.java
+++ java/scrabble-score/src/main/java/Scrabble.java
@@ -1,10 +1,24 @@
+import io.vavr.API;
+import io.vavr.collection.CharSeq;
+
+import static io.vavr.API.$;
+
 class Scrabble {
 
+    private final CharSeq word;
+
     Scrabble(String word) {
+        this.word = CharSeq.of(word);
     }
 
     int getScore() {
-        return 1;
+        return getScore(word.head());
+    }
+
+    private int getScore(Character character) {
+        return API.Match(character).of(
+                API.Case($('a'), 1)
+        );
     }
 
 }
