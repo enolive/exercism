d45e43f8c23d241cd48ac58b4d4818087bb9007f
Christoph Welcz <christoph@welcz.de>
2018-06-18 22:02:38 +0200

    solved with rule set and maybe monad

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,16 +1,23 @@
+import io.vavr.Tuple;
+import io.vavr.collection.List;
+import io.vavr.control.Option;
+
 class RaindropConverter {
 
     String convert(int number) {
-        if (isDivisibleBy(number, 3)) {
-            return "Pling";
-        }
-        if (isDivisibleBy(number, 5)) {
-            return "Plang";
-        }
-        if (isDivisibleBy(number, 7)) {
-            return "Plong";
-        }
-        return Integer.toString(number);
+        var rules = List.of(
+                Tuple.of(isDivisibleBy(number, 3), "Pling"),
+                Tuple.of(isDivisibleBy(number, 5), "Plang"),
+                Tuple.of(isDivisibleBy(number, 7), "Plong")
+        );
+        var result = rules
+                .filter(r -> r._1)
+                .map(r -> r._2)
+                .mkString();
+        return Option
+                .of(result)
+                .filter(s -> !s.isEmpty())
+                .getOrElse(() -> Integer.toString(number));
     }
 
     private boolean isDivisibleBy(int input, int divisor) {
