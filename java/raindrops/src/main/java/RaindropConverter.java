7003b44a15d6456394e44685d0f7aa4754c682d4
Christoph Welcz <christoph@welcz.de>
2018-06-18 22:31:57 +0200

    solved with cycling through an endless list

--- java/raindrops/src/main/java/RaindropConverter.java
+++ java/raindrops/src/main/java/RaindropConverter.java
@@ -1,35 +1,20 @@
 import io.vavr.Function1;
-import io.vavr.Tuple;
-import io.vavr.Tuple2;
-import io.vavr.collection.List;
-import io.vavr.control.Option;
+import io.vavr.collection.Stream;
 
 class RaindropConverter {
 
-    private final List<Tuple2<Function1<Integer, Boolean>, String>> rules = List.of(
-            Tuple.of(numbersDivisibleBy(3), "Pling"),
-            Tuple.of(numbersDivisibleBy(5), "Plang"),
-            Tuple.of(numbersDivisibleBy(7), "Plong")
-    );
+    private static final Function1<Integer, String> SEQUENCE = sequence().memoized();
+
+    private static Stream<String> sequence() {
+        final var plings = Stream.of("", "", "Pling").cycle();
+        final var plangs = Stream.of("", "", "", "", "Plang").cycle();
+        final var plongs = Stream.of("", "", "", "", "", "", "Plong").cycle();
+        final var words = plings.zipWith(plangs, String::concat).zipWith(plongs, String::concat);
+        final var numbers = Stream.from(1).map(String::valueOf);
+        return words.zipWith(numbers, (word, num) -> word.isEmpty() ? num : word);
+    }
 
     String convert(int number) {
-        return getResultFromRules(number)
-                .filter(this::resultNotEmpty)
-                .getOrElse(() -> Integer.toString(number));
-    }
-
-    private Option<String> getResultFromRules(int number) {
-        return Option.of(rules
-                .filter(r -> r._1.apply(number))
-                .map(r -> r._2)
-                .mkString());
-    }
-
-    private boolean resultNotEmpty(String result) {
-        return !result.isEmpty();
-    }
-
-    private Function1<Integer, Boolean> numbersDivisibleBy(int divisor) {
-        return input -> input % divisor == 0;
+        return SEQUENCE.apply(number - 1);
     }
 }
