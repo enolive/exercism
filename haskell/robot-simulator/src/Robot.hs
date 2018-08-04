91027516cad6a306d6f45f6116cab636411019fa
Christoph Welcz <christoph@welcz.de>
2018-08-04 22:01:23 +0200

    made it a little bit better

--- haskell/robot-simulator/src/Robot.hs
+++ haskell/robot-simulator/src/Robot.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Robot
   ( Bearing(East, North, South, West)
   , bearing
@@ -17,32 +19,33 @@ data Bearing
 
 data Robot = Robot
   { bearing :: Bearing
-  , coordinates :: (Integer, Integer)
+  , coordinates :: Position
   }
 
-mkRobot :: Bearing -> (Integer, Integer) -> Robot
+type Position = (Integer, Integer)
+
+mkRobot :: Bearing -> Position -> Robot
 mkRobot direction coordinates = Robot {bearing = direction, coordinates = coordinates}
 
 simulate :: Robot -> String -> Robot
 simulate = foldl (flip move)
 
-move :: Char -> Robot -> Robot
+move :: Char ->  Robot -> Robot
 move 'A' = advance
 move 'L' = changeBearing turnLeft
 move 'R' = changeBearing turnRight
 
 changeBearing :: (Bearing -> Bearing) -> Robot -> Robot
-changeBearing f Robot {bearing = dir, coordinates = location} = mkRobot (f dir) location
+changeBearing f Robot {..} = mkRobot (f bearing) coordinates
 
 advance :: Robot -> Robot
-advance Robot {bearing = dir, coordinates = (x, y)} = mkRobot dir newCoordinates
-  where
-    newCoordinates =
-      case dir of
-        North -> (x, y + 1)
-        South -> (x, y - 1)
-        East -> (x + 1, y)
-        West -> (x - 1, y)
+advance Robot {bearing = dir, coordinates = (x, y)} = mkRobot dir $ changeCoordinates dir (x, y)
+
+changeCoordinates :: Bearing -> Position -> Position
+changeCoordinates North (x, y) = (x, y + 1)
+changeCoordinates South (x, y) = (x, y - 1)
+changeCoordinates East (x, y) = (x + 1, y)
+changeCoordinates West (x, y) = (x - 1, y)
 
 turnLeft :: Bearing -> Bearing
 turnLeft North = West
