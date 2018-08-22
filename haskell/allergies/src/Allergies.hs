4bdb7dcee515bd647037079e6baa1f81de2d146e
Christoph Welcz <christoph@welcz.de>
2018-08-22 16:52:19 +0200

    solved the exercise

--- haskell/allergies/src/Allergies.hs
+++ haskell/allergies/src/Allergies.hs
@@ -1,17 +1,33 @@
-module Allergies (Allergen(..), allergies, isAllergicTo) where
+module Allergies
+  ( Allergen(..)
+  , allergies
+  , isAllergicTo
+  ) where
 
-data Allergen = Eggs
-              | Peanuts
-              | Shellfish
-              | Strawberries
-              | Tomatoes
-              | Chocolate
-              | Pollen
-              | Cats
-              deriving (Eq)
+import           Data.Bits
+
+data Allergen
+  = Eggs
+  | Peanuts
+  | Shellfish
+  | Strawberries
+  | Tomatoes
+  | Chocolate
+  | Pollen
+  | Cats
+  deriving (Eq, Show, Bounded, Enum)
 
 allergies :: Int -> [Allergen]
-allergies score = error "You need to implement this function."
+allergies total = [allergen | (single, allergen) <- allergenScores, matchesScore total single]
+
+matchesScore :: Int -> Int -> Bool
+matchesScore total single = total .&. single /= 0
+
+allergenScores :: [(Int, Allergen)]
+allergenScores = zip bits allAllergens
+  where
+    allAllergens = [(minBound :: Allergen) ..]
+    bits = map (2^) [0..]
 
 isAllergicTo :: Allergen -> Int -> Bool
-isAllergicTo allergen score = error "You need to implement this function."
+isAllergicTo allergen score = allergen `elem` allergies score
