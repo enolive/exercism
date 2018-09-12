60af7166737777db6eebcd34ff7b68a490da5010
Christoph Welcz <christoph@welcz.de>
2018-09-12 12:42:42 +0200

    made window work with infinite lists as well

--- haskell/largest-series-product/src/Series.hs
+++ haskell/largest-series-product/src/Series.hs
@@ -11,6 +11,16 @@ data Error
   deriving (Show, Eq)
 
 largestProduct :: Int -> String -> Either Error Integer
-largestProduct size digits = toInteger . product . toDigits <$> Right digits
+largestProduct size digits = toInteger . highest . products . slidingWindow . toDigits <$> Right digits
   where
     toDigits = map digitToInt
+    products = map product
+    highest = foldl max 0
+    slidingWindow = window size
+
+window :: Int -> [a] -> [[a]]
+window size all@(x:xs)
+  | length part < size = []
+  | otherwise = part : window size xs
+  where
+    part = take size all
