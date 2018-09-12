bbabc99bd091d7b3429e7876e066eae12aa8e9f2
Christoph Welcz <christoph@welcz.de>
2018-09-12 08:41:11 +0200

    some minor refactoring

--- haskell/largest-series-product/src/Series.hs
+++ haskell/largest-series-product/src/Series.hs
@@ -1,8 +1,16 @@
-module Series (Error(..), largestProduct) where
+module Series
+  ( Error(..)
+  , largestProduct
+  ) where
 
-import Data.Char (digitToInt)
+import           Data.Char (digitToInt)
 
-data Error = InvalidSpan | InvalidDigit Char deriving (Show, Eq)
+data Error
+  = InvalidSpan
+  | InvalidDigit Char
+  deriving (Show, Eq)
 
 largestProduct :: Int -> String -> Either Error Integer
-largestProduct size digits = toInteger . product . map digitToInt <$> Right digits
+largestProduct size digits = toInteger . product . toDigits <$> Right digits
+  where
+    toDigits = map digitToInt
