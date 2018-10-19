2ee1100d3ada76e711c6c7eac7568581e420a1c9
Christoph Welcz <christoph@welcz.de>
2018-10-19 19:35:48 +0200

    implemented rows

---
+++ haskell/matrix/src/Matrix.hs
@@ -0,0 +1,55 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Matrix
+  ( Matrix
+  , cols
+  , column
+  , flatten
+  , fromList
+  , fromString
+  , reshape
+  , row
+  , rows
+  , shape
+  , transpose
+  ) where
+
+import qualified Data.Vector as Vector (fromList)
+import Data.Vector (Vector)
+
+newtype Matrix a = Matrix
+  { allRows :: [[a]]
+  } deriving (Eq, Show)
+
+cols :: Matrix a -> Int
+cols matrix = error "You need to implement this function."
+
+column :: Int -> Matrix a -> Vector a
+column x matrix = error "You need to implement this function."
+
+flatten :: Matrix a -> Vector a
+flatten matrix = error "You need to implement this function."
+
+fromList :: [[a]] -> Matrix a
+fromList xss = error "You need to implement this function."
+
+fromString :: Read a => String -> Matrix a
+fromString xs = Matrix allRows
+  where
+    splitted = (map words . lines) xs
+    allRows = map (map read) splitted
+
+reshape :: (Int, Int) -> Matrix a -> Matrix a
+reshape dimensions matrix = error "You need to implement this function."
+
+row :: Int -> Matrix a -> Vector a
+row x Matrix {..} = Vector.fromList $ allRows !! x
+
+rows :: Matrix a -> Int
+rows matrix = error "You need to implement this function."
+
+shape :: Matrix a -> (Int, Int)
+shape matrix = error "You need to implement this function."
+
+transpose :: Matrix a -> Matrix a
+transpose matrix = error "You need to implement this function."
