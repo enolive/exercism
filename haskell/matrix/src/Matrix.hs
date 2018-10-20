83d860bf8be78816871c9bbccfc72cfa69a57cde
Christoph Welcz <christoph@welcz.de>
2018-10-20 18:08:22 +0200

    implemented rows and cols

--- haskell/matrix/src/Matrix.hs
+++ haskell/matrix/src/Matrix.hs
@@ -14,18 +14,20 @@ module Matrix
   , transpose
   ) where
 
-import qualified Data.Vector as Vector (fromList)
-import Data.Vector (Vector)
+import           Control.Arrow ((&&&))
+import           Data.Vector   (Vector)
+import qualified Data.Vector   as Vector (fromList)
 
 newtype Matrix a = Matrix
   { allRows :: [[a]]
   } deriving (Eq, Show)
 
 cols :: Matrix a -> Int
-cols matrix = error "You need to implement this function."
+cols Matrix {allRows = []} = 0
+cols Matrix {..}           = (minimum . map length) allRows
 
 column :: Int -> Matrix a -> Vector a
-column x Matrix{..} = (Vector.fromList . map (!! x)) allRows
+column x Matrix {..} = (Vector.fromList . map (!! x)) allRows
 
 flatten :: Matrix a -> Vector a
 flatten matrix = error "You need to implement this function."
@@ -46,14 +48,10 @@ row :: Int -> Matrix a -> Vector a
 row x Matrix {..} = Vector.fromList $ allRows !! x
 
 rows :: Matrix a -> Int
-rows matrix = error "You need to implement this function."
+rows Matrix {..} = length allRows
 
 shape :: Matrix a -> (Int, Int)
-shape Matrix{..} = (rowLength allRows, colLength allRows)
-  where
-    colLength [] = 0
-    colLength xss = (minimum . map length) xss
-    rowLength xss = length xss
+shape = rows &&& cols
 
 transpose :: Matrix a -> Matrix a
 transpose matrix = error "You need to implement this function."
