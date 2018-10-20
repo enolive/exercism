15681b416b669f6a65d418a3509527af6827fff5
Christoph Welcz <christoph@welcz.de>
2018-10-20 21:56:35 +0200

    finished the exercise!

--- haskell/house/src/House.hs
+++ haskell/house/src/House.hs
@@ -19,7 +19,7 @@ start :: Int -> String
 start num = "This is the " ++ fst (thing num)
 
 remaining :: Int -> [String]
-remaining 1 = ["that " ++ snd (thing 1)]
+remaining 1 = ["that " ++ snd (thing 1) ++ "."]
 remaining num = current : remaining (num - 1)
   where
     current = "that " ++ that ++ " the " ++ the
@@ -35,103 +35,14 @@ things = [ ("house", "Jack built")
          , ("rat", "ate")
          , ("cat", "killed")
          , ("dog", "worried")
-         , ("cow with the crumpled horn", "kicked")
+         , ("cow with the crumpled horn", "tossed")
          , ("maiden all forlorn", "milked")
          , ("man all tattered and torn", "kissed")
          , ("priest all shaven and shorn", "married")
-         , ("rooster that crowed in the morn", "waked")
+         , ("rooster that crowed in the morn", "woke")
          , ("farmer sowing his corn", "kept")
-         , ("horse and the hound and the horn", "belonged")
+         , ("horse and the hound and the horn", "belonged to")
   ]
 
 rhyme :: String
-rhyme =
-  "This is the house that Jack built.\n\
-        \\n\
-        \This is the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n\
-        \\n\
-        \This is the horse and the hound and the horn\n\
-        \that belonged to the farmer sowing his corn\n\
-        \that kept the rooster that crowed in the morn\n\
-        \that woke the priest all shaven and shorn\n\
-        \that married the man all tattered and torn\n\
-        \that kissed the maiden all forlorn\n\
-        \that milked the cow with the crumpled horn\n\
-        \that tossed the dog\n\
-        \that worried the cat\n\
-        \that killed the rat\n\
-        \that ate the malt\n\
-        \that lay in the house that Jack built.\n"
+rhyme = intercalate "\n" $ map (unlines . verse) [1..length things]
