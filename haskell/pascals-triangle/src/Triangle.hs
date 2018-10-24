a6461c2e7d1cf38cc62266796455e613667f6ce8
Christoph Welcz <christoph@welcz.de>
2018-10-24 17:37:24 +0200

    first few tests

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -1,4 +1,9 @@
 module Triangle (rows) where
 
 rows :: Int -> [[Integer]]
+rows 0 = []
+rows 1 = rows 0 ++ [[1]]
+rows 2 = rows 1 ++ [[1, 1]]
+rows 3 = rows 2 ++ [[1, 2, 1]]
+rows 4 = rows 3 ++ [[1, 3, 3, 1]]
 rows x = error "You need to implement this function."
