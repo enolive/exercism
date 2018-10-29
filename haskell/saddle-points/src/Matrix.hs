7011352a46c39af78acc762f1fd8a1dc66a599e2
Christoph Welcz <christoph@welcz.de>
2018-10-29 19:35:21 +0100

    solution with some fancy list comprehension

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -1,8 +1,15 @@
-module Matrix (saddlePoints) where
+module Matrix
+  ( saddlePoints
+  ) where
 
-import Data.Array (Array, indices)
+import           Data.Array (Array, bounds, (!))
 
 type Index = (Int, Int)
 
-saddlePoints :: Array Index e -> [Index]
-saddlePoints matrix = [(1,0)]
+saddlePoints :: Ord e => Array Index e -> [Index]
+saddlePoints matrix =
+  [(a, b) | a <- [minX .. maxX], b <- [minY .. maxY], matrix ! (a, b) == maxRow a, matrix ! (a, b) == minCol b]
+  where
+    ((minX, minY), (maxX, maxY)) = bounds matrix
+    maxRow a = maximum [matrix ! (a, y) | y <- [minY .. maxY]]
+    minCol b = minimum [matrix ! (x, b) | x <- [minX .. maxX]]
