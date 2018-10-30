f8d04d6425e6be16ea2237ca0bcd7fd672d88797
Christoph Welcz <christoph@welcz.de>
2018-10-30 22:12:37 +0100

    made even shorter by using indices ;-)

--- haskell/saddle-points/src/Matrix.hs
+++ haskell/saddle-points/src/Matrix.hs
@@ -7,7 +7,10 @@ import           Data.Array (Array, bounds, indices, range, (!))
 type Index = (Int, Int)
 
 saddlePoints :: Ord e => Array Index e -> [Index]
-saddlePoints matrix = [index | index@(a, b) <- indices matrix, matrix ! index == maxRow a, matrix ! index == minCol b]
+saddlePoints matrix = [index
+  | index@(a, b) <- indices matrix
+  , matrix ! index == maxRow a
+  , matrix ! index == minCol b]
   where
     ((minX, minY), (maxX, maxY)) = bounds matrix
     maxRow a = maximum [matrix ! (a, y) | y <- [minY .. maxY]]
