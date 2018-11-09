3a23c877de0d9230296877d3361fa3a9f8090804
Christoph Welcz <christoph@welcz.de>
2018-11-09 13:45:53 +0100

    extracted function for parsing digits

--- haskell/series/src/Series.hs
+++ haskell/series/src/Series.hs
@@ -3,11 +3,14 @@ module Series (slices) where
 import Data.Char (digitToInt)
 
 slices :: Int -> String -> [[Int]]
-slices n = map (map digitToInt) . slidingWindow n
+slices n = map strToInt . slidingWindow n
+
+strToInt :: String -> [Int]
+strToInt = map digitToInt
 
 slidingWindow :: Int -> [a] -> [[a]]
 slidingWindow 0 [] = [[]]
 slidingWindow n xs
   | null xs = []
   | length xs < n = []
-  | otherwise = (take n) xs : (slidingWindow n . drop 1) xs
+  | otherwise = take n xs : (slidingWindow n . drop 1) xs
